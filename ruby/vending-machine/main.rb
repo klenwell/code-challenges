07e3f5633896940b6795fc8b066856e29f38cd81
Tom Atwell <klenwell@gmail.com>
2020-11-10 18:24:18 -0800

    Cleans up rubocop violations in main file.

--- ruby/vending-machine/main.rb
+++ ruby/vending-machine/main.rb
@@ -17,19 +17,22 @@ p product, change, vm.display
 # Insufficient change
 vm.update_tray_price('A1', 50)
 vm.insert_coin(100)
-product, change = vm.select_product('A1')
+vm.select_product('A1')
 p vm.display
 
 # Reload and try again
-vm.coin_sorter.load({1 => 40, 2 => 10, 5 => 4, 10 => 2})
+vm.coin_sorter.load({ 1 => 40, 2 => 10, 5 => 4, 10 => 2 })
 vm.insert_coin(100)
 vm.select_product('A1')
 print vm.status
 
 # Order
-[100, 20].each{|coin| vm.insert_coin(coin)}
+[100, 20].each { |coin| vm.insert_coin(coin) }
 product, change = vm.select_product('A1')
 
 p "VM dispensed: #{product}; Your change: #{change}"
 print vm.status
 p vm
+
+# Go interactive
+binding.pry
