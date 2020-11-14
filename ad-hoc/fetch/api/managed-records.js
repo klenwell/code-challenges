ed9ee034be0fffc30a53761e70b86b4218363d52
klenwell <klenwell@gmail.com>
2020-11-14 11:08:57 -0800

    Removes debug log calls and cleans up code. Now passes all tests.

--- ad-hoc/fetch/api/managed-records.js
+++ ad-hoc/fetch/api/managed-records.js
@@ -6,6 +6,7 @@ window.path = "http://localhost:3000/records";
 
 // Constants
 const primaryColors = ['red', 'blue', 'yellow'];
+const pageLimit = 10;
 
 // Your retrieve function plus any additional functions go here ...
 function retrieve(options = {}) {
@@ -14,40 +15,12 @@ function retrieve(options = {}) {
   let colors = options.colors || [];
 
   // Build URI
-  let limit = 10;
-  let offset = (page - 1) * limit;
-  let uri = URI(window.path);
-
-  // Get 1 extra record to see if we're on our last page.
-  uri.search({ limit: limit + 1, offset: offset });
-
-  if (colors.length > 0) {
-    colors.forEach(color => uri.addSearch('color[]', color));
-  }
-  console.log(uri.toString())
+  let uri = buildUri(page, colors);
 
   // Docs: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
   return fetch(uri)
-  .then(response => {
-    console.log(response.status);
-    return response.json()
-  })
-  .then(records => {
-    let isLastPage = records.length <= limit;
-    records = records.slice(0, 10);
-    console.log(records.length, records[0], isLastPage);
-
-    let payload = {};
-    payload.ids = records.map(record => record.id);
-    payload.open = records.filter(record => record.disposition == 'open');
-    payload.open.forEach(record => record.isPrimary = primaryColors.includes(record.color));
-    payload.closedPrimaryCount = records.filter(record => isPrimaryClosed(record)).length;
-    payload.previousPage = page == 1 ? null : page - 1;
-    payload.nextPage = isLastPage ? null : page + 1;
-    console.log(payload);
-
-    return payload;
-  })
+  .then(response => { return response.json() })
+  .then(records => { return recordsToPayload(records, page) })
   .catch((error) => {
     console.log('Error:', error);
     return null;
@@ -55,6 +28,39 @@ function retrieve(options = {}) {
 }
 
 // Helper methods
+function buildUri(page, colors) {
+  let limit = pageLimit + 1; // Include 1 extra record to test if we're on our page.
+  let offset = (page - 1) * pageLimit;
+  let uri = URI(window.path);
+
+  // Docs: https://medialize.github.io/URI.js/
+  uri.search({ limit: limit, offset: offset });
+  colors.forEach(color => uri.addSearch('color[]', color));
+
+  return uri;
+}
+
+function recordsToPayload(records, page) {
+  // What happens if total records are a multiple of 10? This could fail.
+  let isLastPage = records.length <= pageLimit;
+
+  // Grab a page full of records.
+  records = records.slice(0, 10);
+
+  let payload = {
+    ids: records.map(record => record.id),
+    open: records.filter(record => record.disposition == 'open'),
+    previousPage: page == 1 ? null : page - 1,
+    nextPage: isLastPage ? null : page + 1
+  };
+
+  // Add isPrimary property to records and count.
+  payload.open.forEach(record => record.isPrimary = primaryColors.includes(record.color));
+  payload.closedPrimaryCount = records.filter(record => isPrimaryClosed(record)).length;
+
+  return payload;
+}
+
 function isPrimaryClosed(record) {
   return record.disposition == 'closed' && primaryColors.includes(record.color);
 }
