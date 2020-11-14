3a960c5fcad949c849eacc0f9c4496fef292a958
klenwell <klenwell@gmail.com>
2020-11-13 20:05:21 -0800

    Stubs out bones of the fetch operation and transformation.

--- ad-hoc/fetch/api/managed-records.js
+++ ad-hoc/fetch/api/managed-records.js
@@ -6,7 +6,22 @@ window.path = "http://localhost:3000/records";
 
 // Your retrieve function plus any additional functions go here ...
 function retrieve(options) {
-  throw new Error("TODO");
+  uri = URI(window.path)
+
+  // Docs: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+  return fetch(uri)
+  .then(response => {
+    console.log(response.status);
+    return response.json()
+  })
+  .then(data => {
+    console.log(data.length, data[0]);
+    record = {};
+    return record;
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 }
 
 export default retrieve;
