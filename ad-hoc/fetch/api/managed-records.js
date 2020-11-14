d0d72ce5af6863595b71b5d2a8e967c9cf9b8809
klenwell <klenwell@gmail.com>
2020-11-13 21:48:09 -0800

    Builds basic unfiltered payload object.

--- ad-hoc/fetch/api/managed-records.js
+++ ad-hoc/fetch/api/managed-records.js
@@ -4,9 +4,12 @@ import URI from "urijs";
 // /records endpoint
 window.path = "http://localhost:3000/records";
 
+// Constants
+const primaryColors = ['red', 'blue', 'yellow'];
+
 // Your retrieve function plus any additional functions go here ...
 function retrieve(options) {
-  uri = URI(window.path)
+  let uri = URI(window.path)
 
   // Docs: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
   return fetch(uri)
@@ -14,14 +17,23 @@ function retrieve(options) {
     console.log(response.status);
     return response.json()
   })
-  .then(data => {
-    console.log(data.length, data[0]);
-    record = {};
-    return record;
+  .then(records => {
+    console.log(records.length, records[0]);
+    let payload = {};
+    payload.ids = records.map(record => record.id);
+    payload.open = records.filter(record => record.disposition == 'open');
+    payload.open.forEach(record => record.isPrimary = primaryColors.includes(record.color));
+    payload.closedPrimaryCount = records.filter(record => record.disposition == 'closed').length;
+    payload.previousPage = null;
+    payload.nextPage = null;
+    console.log(payload);
+    return payload;
   })
   .catch((error) => {
     console.error('Error:', error);
   });
 }
+
+// Helper methods
 
 export default retrieve;
