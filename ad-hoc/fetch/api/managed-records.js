091d11572dcb2622124cb5be16f3b7ad2511aaeb
klenwell <klenwell@gmail.com>
2020-11-13 22:02:51 -0800

    Passes first 2 tests. Just 9 to go.

--- ad-hoc/fetch/api/managed-records.js
+++ ad-hoc/fetch/api/managed-records.js
@@ -11,6 +11,8 @@ const primaryColors = ['red', 'blue', 'yellow'];
 function retrieve(options) {
   let uri = URI(window.path)
 
+  uri.search({ page: 1, colors: [] });
+
   // Docs: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
   return fetch(uri)
   .then(response => {
@@ -18,14 +20,15 @@ function retrieve(options) {
     return response.json()
   })
   .then(records => {
+    records = records.slice(0, 10);
     console.log(records.length, records[0]);
     let payload = {};
     payload.ids = records.map(record => record.id);
     payload.open = records.filter(record => record.disposition == 'open');
     payload.open.forEach(record => record.isPrimary = primaryColors.includes(record.color));
-    payload.closedPrimaryCount = records.filter(record => record.disposition == 'closed').length;
+    payload.closedPrimaryCount = records.filter(record => isPrimaryClosed(record)).length;
     payload.previousPage = null;
-    payload.nextPage = null;
+    payload.nextPage = 2;
     console.log(payload);
     return payload;
   })
@@ -35,5 +38,8 @@ function retrieve(options) {
 }
 
 // Helper methods
+function isPrimaryClosed(record) {
+  return record.disposition == 'closed' && primaryColors.includes(record.color);
+}
 
 export default retrieve;
