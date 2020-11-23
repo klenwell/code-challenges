285cdaabcc561939df5277ef0f4fde55faf37bf1
klenwell <klenwell@gmail.com>
2020-11-22 21:07:54 -0800

    Adds code for Buttons example from guide.

---
+++ elm/guide/src/Buttons.elm
@@ -0,0 +1,52 @@
+--
+-- To compile:
+-- elm make src/Buttons.elm --output=buttons.js
+--
+module Buttons exposing (..)
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+type alias Model = Int
+
+init : Model
+init =
+  0
+
+
+-- UPDATE
+
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (String.fromInt model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
