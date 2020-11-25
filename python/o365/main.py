31118c7f2a12305263cf5f3414c8ca4a0dc4c5ae
klenwell <klenwell@gmail.com>
2020-11-25 14:09:28 -0800

    Adds method to fetch emails and updates authenticate method to authenticate only when token is absent or expired.

--- python/o365/main.py
+++ python/o365/main.py
@@ -1,6 +1,7 @@
 from O365 import Account, MSGraphProtocol
 from secrets import CLIENT_ID, SECRET_ID
 from datetime import datetime
+from pathlib import Path
 
 
 #
@@ -16,17 +17,49 @@ def calendar_events():
     schedule = account.schedule()
     calendar = schedule.get_default_calendar()
 
-    start_date = datetime(2020, 1, 1)
-    end_date = datetime(2020, 12, 31)
-    q = calendar.new_query('start').greater_equal(start_date)
-    q.chain('and').on_attribute('end').less_equal(end_date)
-
-    events = calendar.get_events(query=q, include_recurring=True)
+    #start_date = datetime(2020, 1, 1)
+    #end_date = datetime(2020, 12, 31)
+    #q = calendar.new_query('start').greater_equal(start_date)
+    #q.chain('and').on_attribute('end').less_equal(end_date)
+    #events = calendar.get_events(query=q, include_recurring=True)
+    events = calendar.get_events(include_recurring=False)
     events = list(events)
+    event = events[0]
 
     print(len(events))
     print(events)
+    #print(event.attendees[0].address)
     breakpoint()
+
+    return events
+
+
+def email_messages():
+    """Based on
+    https://github.com/O365/python-o365#mailbox
+    """
+    scopes = ['Mail.Read']
+    account = authenticate(scopes)
+
+    mailbox = account.mailbox()
+    inbox = mailbox.inbox_folder()
+    inbox_messages = list(inbox.get_messages())
+    print(len(inbox_messages))
+
+    sent_folder = mailbox.sent_folder()
+    sent_messages = list(sent_folder.get_messages())
+    print(len(sent_messages))
+
+    messages = inbox_messages + sent_messages
+    message = message[0]
+
+    print(len(messages))
+    print(messages)
+    print(message.to_api_data().keys())
+    #print(message.to[0].address)
+    breakpoint()
+
+    return messages
 
 
 #
@@ -37,10 +70,19 @@ def authenticate(scopes):
     protocol = MSGraphProtocol()
     account = Account(credentials, protocol=protocol)
 
-    if account.authenticate(scopes=scopes):
+    if account.is_authenticated:
+        print("Token file exists!")
+        # account.connection.refresh_token()
+    else:
+        account.authenticate(scopes=scopes)
         print('Authenticated!')
 
     return account
+
+def token_file_exists():
+    TOKEN_FILE = 'o365_token.txt'
+    token_path = Path(TOKEN_FILE)
+    return token_path.is_file()
 
 
 #
