a31fb1e33e6fecfb32c17d5db8ee2b5cea0dd63f
klenwell <klenwell@gmail.com>
2020-12-15 14:52:27 -0800

    Stubs out view model and HouseholdMember class.

--- javascript/knockout/households/index.js
+++ javascript/knockout/households/index.js
@@ -1,1 +1,31 @@
 // your code goes here ...
+function HouseholdMember(age, relationship, smokes) {
+  let member = this
+
+  member.age = age
+  member.relationship = relationship
+  member.smokes = smokes
+
+  member.asListItem = ko.computed(function() {
+    const smokingIcon = '🚬'
+    const noSmokingIcon = '🚭'
+    const smokesIcon = member.smokes ? smokingIcon : noSmokingIcon
+    return `${member.relationship}: ${member.age} ${smokesIcon}`
+  })
+}
+
+function HouseholdsViewModel() {
+  let model = this
+
+  const validRelationships = ['self', 'spouse', 'child', 'parent', 'grandparent', 'other']
+
+  model.members = ko.observableArray([
+    new HouseholdMember(40, validRelationships[0], false)
+  ])
+
+  model.addMember = () => { console.log("TODO: addMember") }
+
+  model.submit = () => { console.log("TODO: submit") }
+}
+
+ko.applyBindings(new HouseholdsViewModel());
