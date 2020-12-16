78468f35f2870476501c1eed457a1ea01b9215ea
klenwell <klenwell@gmail.com>
2020-12-16 09:03:47 -0800

    Applies standard.js linter fixes.

--- javascript/knockout/households/index.js
+++ javascript/knockout/households/index.js
@@ -1,12 +1,12 @@
 // your code goes here ...
-function HouseholdMember(age, relationship, smokes) {
-  let member = this
+function HouseholdMember (age, relationship, smokes) {
+  const member = this
 
   member.age = age
   member.relationship = relationship
   member.smokes = smokes
 
-  member.asListItem = ko.computed(function() {
+  member.asListItem = ko.computed(function () {
     const smokingIcon = '🚬'
     const noSmokingIcon = '🚭'
     const smokesIcon = member.smokes ? smokingIcon : noSmokingIcon
@@ -14,40 +14,40 @@ function HouseholdMember(age, relationship, smokes) {
   })
 }
 
-function HouseholdsViewModel() {
-  let model = this
+function HouseholdsViewModel () {
+  const model = this
 
   model.validRelationships = ['self', 'spouse', 'child', 'parent', 'grandparent', 'other']
   model.members = ko.observableArray([new HouseholdMember(40, 'self', false)])
-  model.memberAge = ko.observable().extend({required: true, min: 1})
-  model.memberRelationship = ko.observable().extend({required: true})
+  model.memberAge = ko.observable().extend({ required: true, min: 1 })
+  model.memberRelationship = ko.observable().extend({ required: true })
   model.memberSmokes = ko.observable()
   model.debugText = ko.observable()
   model.toggleDebug = ko.observable(false)
 
   model.addMember = (el, event) => {
-    if ( ! model.isValid() ) {
+    if (!model.isValid()) {
       model.displayErrors()
       return
     }
 
-    let age = model.memberAge()
-    let rel = model.memberRelationship()
-    let smokes = model.memberSmokes()
-    let newMember = new HouseholdMember(age, rel, smokes)
+    const age = model.memberAge()
+    const rel = model.memberRelationship()
+    const smokes = model.memberSmokes()
+    const newMember = new HouseholdMember(age, rel, smokes)
 
     model.members.push(newMember)
     model.resetForm()
   }
 
   model.removeMember = (member) => {
-    if ( confirm("Remove this member?") ) {
+    if (confirm('Remove this member?')) {
       model.members.remove(member)
     }
   }
 
   model.submitForm = () => {
-    let jsonMembers = ko.toJSON(model.members())
+    const jsonMembers = ko.toJSON(model.members())
 
     model.debugText(jsonMembers)
     model.toggleDebug(true)
