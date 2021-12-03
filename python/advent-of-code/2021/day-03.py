6d41a3e2a1159a7a86e78060cd9a9f892aa07ec4
klenwell <klenwell@gmail.com>
2021-12-03 06:48:12 -0800

    Solves AOC day 3 part 2. Need to clean up.

--- python/advent-of-code/2021/day-03.py
+++ python/advent-of-code/2021/day-03.py
@@ -41,7 +41,7 @@ def parse_bit_columns(bit_columns):
 
     for bit_list in bit_columns:
         gamma_bit = bit_list_to_gamma_bit(bit_list)
-        epsilon_bit = 1 if gamma_bit == 0 else 0
+        epsilon_bit = 1 if gamma_bit == 0 else 0    # or gamma_bit ^ 1
         gamma_bits.append(gamma_bit)
         epsilon_bits.append(epsilon_bit)
 
@@ -61,6 +61,62 @@ def bit_list_to_gamma_bit(bit_list):
         return 1
 
 
+def rate_rows(rows, match_by, default_bit):
+    num_cols = len(rows[0])
+    reduced_rows = rows.copy()
+
+    if match_by == 'most':
+        match_bit_fx = most_common_bit_by_index
+    else:
+        match_bit_fx = least_common_bit_by_index
+
+
+    for col in range(num_cols):
+        match_bit = match_bit_fx(reduced_rows, col)
+        matching_rows = []
+        print(col, len(reduced_rows), reduced_rows[0], match_bit)
+
+        match_bit = match_bit if match_bit != 2 else default_bit
+
+        for row in reduced_rows:
+            bits = [int(b) for b in list(row)]
+            if bits[col] == match_bit:
+                matching_rows.append(row)
+
+        reduced_rows = matching_rows.copy()
+        print(len(reduced_rows))
+
+        if len(reduced_rows) == 1:
+            break
+
+    to_bit_str = lambda bits: ''.join([str(b) for b in bits])
+    return int(to_bit_str(reduced_rows[0]), 2)
+
+
+def extract_co2_rating(rows):
+    pass
+
+
+def most_common_bit_by_index(rows, index):
+    index_bits = [int(row[index]) for row in rows]
+    zero_count = len([bit for bit in index_bits if bit == 0])
+    one_count = len([bit for bit in index_bits if bit == 1])
+
+    if zero_count > one_count:
+        return 0
+    elif one_count > zero_count:
+        return 1
+    else:
+        return 2
+
+def least_common_bit_by_index(rows, index):
+    bit = most_common_bit_by_index(rows, index)
+    if bit == 2:
+        return bit
+    else:
+        return bit ^ 1
+
+
 def solve_pt1():
     rows = extract_report()
     bit_columns = rows_to_bit_columns(rows)
@@ -70,7 +126,10 @@ def solve_pt1():
 
 def solve_pt2():
     """https://adventofcode.com/2021/day/3#part2"""
-    pass
+    rows = extract_report()
+    oxy_rating = rate_rows(rows, 'most', 1)
+    co2_rating = rate_rows(rows, 'least', 0)
+    return oxy_rating * co2_rating
 
 
 #
