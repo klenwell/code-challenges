9545add2f4e5dcb76725fdd13ab08b61bbebef03
klenwell <klenwell@gmail.com>
2021-12-05 16:23:51 -0800

    Solve AOC 2020 day 3. Part 2 took 3 tries to get everything tweaked right. :[

--- python/advent-of-code/2020/day-03.py
+++ python/advent-of-code/2020/day-03.py
@@ -6,8 +6,11 @@ from os.path import join as path_join
 from functools import cached_property
 from config import INPUT_DIR
 
+import math
+
 
 INPUT_FILE = path_join(INPUT_DIR, 'day-03.txt')
+TREE = '#'
 
 
 class Solution:
@@ -17,23 +20,66 @@ class Solution:
     #
     # Properties
     #
+    @property
+    def first(self):
+        dx = 3
+        x = 0
+        trees = 0
+
+        return self.traverse_map(dx, 1)
+
+
+    @property
+    def second(self):
+        slopes = [
+            (1, 1),
+            (3, 1),
+            (5, 1),
+            (7, 1),
+            (1, 2)
+        ]
+        trees_per_slope = []
+
+        for dx, dy in slopes:
+            trees = self.traverse_map(dx, dy)
+            trees_per_slope.append(trees)
+
+        print(trees_per_slope)
+
+        return math.prod(trees_per_slope)
+
     @cached_property
     def input_lines(self):
         with open(self.input_file) as file:
             lines = file.readlines()
             return [line.strip() for line in lines]
 
-    @property
-    def first(self):
-        pass
-
-    @property
-    def second(self):
-        pass
-
     #
     # Methods
     #
+    def map_row(self, line, x):
+        mapped_row = list(line)
+
+        if x >= len(mapped_row):
+            m = x // len(mapped_row) + 1
+            mapped_row *= m
+
+        return mapped_row
+
+    def traverse_map(self, dx, dy):
+        x = 0
+        map_lines = self.input_lines[dy:]
+        hit_trees = 0
+
+        # Step by dy: https://stackoverflow.com/a/2990281/1093087
+        for line in map_lines[::dy]:
+            x += dx
+            row = self.map_row(line, x)
+
+            if row[x] == TREE:
+                hit_trees += 1
+
+        return hit_trees
 
 
 #
