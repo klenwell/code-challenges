6ff81d81b8efe8bc88863a8f8387d940e6b629ad
klenwell <klenwell@gmail.com>
2021-12-05 16:29:03 -0800

    Stub out AOC 2020 day 4.

---
+++ python/advent-of-code/2020/day-04.py
@@ -0,0 +1,44 @@
+"""
+Advent of Code 2020 - Day 04
+https://adventofcode.com/2020/day/4
+"""
+from os.path import join as path_join
+from functools import cached_property
+from config import INPUT_DIR
+
+
+INPUT_FILE = path_join(INPUT_DIR, 'day-04.txt')
+
+
+class Solution:
+    def __init__(self, input_file):
+        self.input_file = input_file
+
+    #
+    # Properties
+    #
+    @property
+    def first(self):
+        pass
+
+    @property
+    def second(self):
+        pass
+
+    @cached_property
+    def input_lines(self):
+        with open(self.input_file) as file:
+            lines = file.readlines()
+            return [line.strip() for line in lines]
+
+    #
+    # Methods
+    #
+    
+
+#
+# Main
+#
+solution = Solution(INPUT_FILE)
+print("pt 1 solution: {}".format(solution.first))
+print("pt 2 solution: {}".format(solution.second))
