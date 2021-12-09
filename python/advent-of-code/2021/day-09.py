43e4222284b2e800edbc3dc6820058ebb0320de6
klenwell <klenwell@gmail.com>
2021-12-09 06:52:19 -0800

    Solve AOC 2021 Day 9 part 1.

--- python/advent-of-code/2021/day-09.py
+++ python/advent-of-code/2021/day-09.py
@@ -19,7 +19,16 @@ class Solution:
     #
     @property
     def first(self):
-        pass
+        risk_level = 0
+
+        for x in range(self.row_count):
+            for y in range(self.column_count):
+                if self.is_low_point(x, y):
+                    risk = self.grid[(x, y)]
+                    print(x, y, risk)
+                    risk_level += risk + 1
+
+        return risk_level
 
     @property
     def second(self):
@@ -31,9 +40,40 @@ class Solution:
             lines = file.readlines()
             return [line.strip() for line in lines]
 
+    @cached_property
+    def grid(self):
+        grid_map = {}
+        for x, line in enumerate(self.input_lines):
+            cols = list(line)
+            for y, val in enumerate(cols):
+                grid_map[(x, y)] = int(val)
+        return grid_map
+
+    @cached_property
+    def row_count(self):
+        return len(self.input_lines)
+
+    @cached_property
+    def column_count(self):
+        return len(self.input_lines[0])
+
     #
     # Methods
     #
+    def is_low_point(self, x, y):
+        default_value = 100
+
+        pt = self.grid[(x, y)]
+        n = self.grid.get((x, y-1), default_value)
+        s = self.grid.get((x, y+1), default_value)
+        e = self.grid.get((x+1, y), default_value)
+        w = self.grid.get((x-1, y), default_value)
+
+        for dir in (n, s, e, w):
+            if pt >= dir:
+                return False
+
+        return True
 
 
 #
