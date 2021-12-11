6c8564cebdfc5383d26ab65d8b3e6b0d893ecbe9
klenwell <klenwell@gmail.com>
2021-12-11 14:06:34 -0800

    Solve AOC 2020 Day 5 puzzle.

--- python/advent-of-code/2020/day-05.py
+++ python/advent-of-code/2020/day-05.py
@@ -19,11 +19,16 @@ class Solution:
     #
     @property
     def first(self):
-        pass
+        return max(self.seat_ids)
 
     @property
     def second(self):
-        pass
+        for left_seat in sorted(self.seat_ids):
+            middle_seat = left_seat + 1
+            right_seat = left_seat + 2
+            print(left_seat, middle_seat, right_seat)
+            if (middle_seat not in self.seat_ids) and (right_seat in self.seat_ids):
+                return middle_seat
 
     @cached_property
     def input_lines(self):
@@ -31,9 +36,30 @@ class Solution:
             lines = file.readlines()
             return [line.strip() for line in lines]
 
+    @property
+    def seat_ids(self):
+        seat_ids = []
+        for seat_code in self.input_lines:
+            seat_id = self.seat_code_to_id(seat_code)
+            seat_ids.append(seat_id)
+        return seat_ids
+
     #
     # Methods
     #
+    def seat_code_to_id(self, code):
+        # Compute row
+        row_code = code[:7]
+        row_bin = ''.join(['1' if c == 'B' else '0' for c in row_code])
+        row = int(row_bin, 2)
+
+        # Compute column
+        col_code = code[7:]
+        col_bin = ''.join(['1' if c == 'R' else '0' for c in col_code])
+        col = int(col_bin, 2)
+
+        # Compute id
+        return row * 8 + col
 
 
 #
