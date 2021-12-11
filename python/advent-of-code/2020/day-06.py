b428ecb52cbfac48d8b24827d81e6e1725e72ef5
klenwell <klenwell@gmail.com>
2021-12-11 14:18:49 -0800

    Prep AOC 2020 Day 6 solution.

---
+++ python/advent-of-code/2020/day-06.py
@@ -0,0 +1,49 @@
+"""
+Advent of Code 2020 - Day 6
+https://adventofcode.com/2021/day/6
+"""
+from os.path import join as path_join
+from functools import cached_property
+from config import INPUT_DIR
+
+
+DAY_NUM = 6
+FNAME = 'day-{:02d}.txt'.format(DAY_NUM)
+INPUT_FILE = path_join(INPUT_DIR, FNAME)
+
+
+class Solution:
+    def __init__(self, input_file):
+        self.input_file = input_file
+
+    #
+    # Solutions
+    #
+    @property
+    def first(self):
+        pass
+
+    @property
+    def second(self):
+        pass
+
+    #
+    # Properties
+    #
+    @cached_property
+    def input_lines(self):
+        with open(self.input_file) as file:
+            lines = file.readlines()
+            return [line.strip() for line in lines]
+
+    #
+    # Methods
+    #
+
+
+#
+# Main
+#
+solution = Solution(INPUT_FILE)
+print("pt 1 solution: {}".format(solution.first))
+print("pt 2 solution: {}".format(solution.second))
