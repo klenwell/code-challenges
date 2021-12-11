94ed60864b3cd2c947b047fcba03337cdb8dc101
klenwell <klenwell@gmail.com>
2021-12-10 19:24:29 -0800

    Stubs out AOC 2021 Day 11.

---
+++ python/advent-of-code/2021/day-11.py
@@ -0,0 +1,47 @@
+"""
+Advent of Code 2021 - Day 11
+https://adventofcode.com/2021/day/11
+"""
+from os.path import join as path_join
+from functools import cached_property
+from config import INPUT_DIR
+
+
+INPUT_FILE = path_join(INPUT_DIR, 'day-10.txt')
+
+
+class Solution:
+    def __init__(self, input_file):
+        self.input_file = input_file
+
+    #
+    # Solutions
+    #
+    @property
+    def first(self):
+        pass
+
+    @property
+    def second(self):
+        pass
+
+    #
+    # Properties
+    #
+    @cached_property
+    def input_lines(self):
+        with open(self.input_file) as file:
+            lines = file.readlines()
+            return [line.strip() for line in lines]
+
+    #
+    # Methods
+    #
+    
+
+#
+# Main
+#
+solution = Solution(INPUT_FILE)
+print("pt 1 solution: {}".format(solution.first))
+print("pt 2 solution: {}".format(solution.second))
