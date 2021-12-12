2066a7b94da9589ddac067c6c4669475f565bd88
klenwell <klenwell@gmail.com>
2021-12-11 22:08:58 -0800

    Failed Day 12 part 1. Too low (1379).

--- python/advent-of-code/2021/day-12.py
+++ python/advent-of-code/2021/day-12.py
@@ -21,7 +21,66 @@ class Solution:
     #
     @property
     def first(self):
-        pass
+        paths = []
+        complete_paths = []
+        complete = False
+        print(self.starters)
+
+        for edge in self.starters:
+            path = ['start']
+            next = self.pop_edge_partner(edge, 'start')
+            path.append(next)
+            paths.append(path)
+
+        while not complete:
+            new_paths = []
+            for path in paths:
+                valid_paths = self.filter_valid_paths(path)
+                new_paths += valid_paths
+
+            complete = all([path[-1] == 'end' for path in new_paths])
+            paths = new_paths
+            print(paths)
+
+        return len(paths)
+
+    def pop_edge_partner(self, edge, node):
+        print(edge, node)
+        pair = edge.copy()
+        pair.remove(node)
+        return pair[0]
+
+    def filter_valid_paths(self, path):
+        new_paths = []
+        last_cave = path[-1]
+        penult_cave = path[-2]
+
+        # No more routes if this path at end
+        if last_cave == 'end':
+            return [path]
+
+        # Find connecting nodes
+        for edge in self.edges:
+            # Skip unconnected path
+            if last_cave not in edge:
+                continue
+
+            # Skip current edge
+            if penult_cave in edge:
+                continue
+
+            next_cave = self.pop_edge_partner(edge, last_cave)
+
+            # Skip small caves that have already been visits
+            if next_cave.islower() and next_cave in path:
+                continue
+
+            # Create new path
+            new_path = path.copy()
+            new_path.append(next_cave)
+            new_paths.append(new_path)
+
+        return new_paths
 
     @property
     def second(self):
@@ -35,6 +94,31 @@ class Solution:
         with open(self.input_file) as file:
             lines = file.readlines()
             return [line.strip() for line in lines]
+
+    @cached_property
+    def edges(self):
+        pairs = []
+        for line in self.input_lines:
+            pair = line.split('-')
+            pairs.append(pair)
+        return pairs
+
+    @cached_property
+    def starters(self):
+        starts = []
+        for pair in self.edges:
+            if 'start' in pair:
+                starts.append(pair)
+        return starts
+
+    @cached_property
+    def enders(self):
+        ends = []
+        for pair in self.edges:
+            if 'end' in pair:
+                ends.append(pair)
+        return ends
+
 
     #
     # Methods
