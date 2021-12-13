ce970420c7243fb8e4b8127bd96f06ed2962dfa2
klenwell <klenwell@gmail.com>
2021-12-12 22:01:46 -0800

    Solve AOC 2021 Day 13 part 1. Got it wrong twice. Forgot to commit code for wrong answers.

--- python/advent-of-code/2021/day-13.py
+++ python/advent-of-code/2021/day-13.py
@@ -19,11 +19,62 @@ class Solution:
     #
     @property
     def first(self):
-        pass
+        grid = {}
+        mirror_grid = {}
+        first_fold = self.folds[0]
+
+        for pt in self.dots:
+            if pt[0] < first_fold[1]:
+                grid[pt] = 'x'
+            elif pt[0] > first_fold[1]:
+                mirror_grid[pt] = 'x'
+
+        print(sorted(grid.keys()))
+        print(sorted(mirror_grid.keys()))
+        print(first_fold)
+
+        axis, pos = first_fold
+        overlay_grid = self.fold_grid_over(mirror_grid, pos)
+        merged_grid = self.overlay_grid(grid, overlay_grid)
+        dots = len(merged_grid.keys())
+
+        assert max([x for (x, y) in merged_grid.keys()]) <= pos
+        print(sorted(merged_grid.keys()))
+
+        # wrong: 464
+        # 958
+        return dots
+
+    def overlay_grid(self, grid, overlay):
+        new_grid = dict(grid)
+
+        for pt, dot in overlay.items():
+            new_grid[pt] = dot
+
+        return new_grid
+
+
+    def fold_grid_over(self, grid, width):
+        folded_grid = {}
+        print(sorted(grid.keys()))
+        for (x, y) in grid:
+            xf = width - x + width
+            if xf >= 0:
+                folded_grid[(xf, y)] = 'x'
+
+        return folded_grid
 
     @property
     def second(self):
         pass
+
+    @property
+    def grid_width(self):
+        return len(self.input_lines[0])
+
+    @property
+    def grid_height(self):
+        return len(self.input_lines)
 
     #
     # Properties
@@ -33,6 +84,34 @@ class Solution:
         with open(self.input_file) as file:
             lines = file.readlines()
             return [line.strip() for line in lines]
+
+    @cached_property
+    def dots(self):
+        dots = []
+        for line in self.input_lines:
+            if not line:
+                break
+
+            x, y = line.split(',')
+            dots.append((int(x), int(y)))
+        return dots
+
+    @cached_property
+    def folds(self):
+        folds = []
+        start_parsing = False
+
+        for line in self.input_lines:
+            if not start_parsing:
+                if not line:
+                    start_parsing = True
+
+                continue
+
+            _, _, inst = line.split(' ')
+            axis, val = inst.split('=')
+            folds.append((axis, int(val)))
+        return folds
 
     #
     # Methods
