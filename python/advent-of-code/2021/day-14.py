daed0d654b0aef0eaf9ad48b7a39e52367212186
klenwell <klenwell@gmail.com>
2021-12-13 21:24:19 -0800

    Wrong answer Day 14 pt 1: 131778.

--- python/advent-of-code/2021/day-14.py
+++ python/advent-of-code/2021/day-14.py
@@ -12,15 +12,50 @@ POLYMER_TEMPLATE = 'CNBPHFBOPCSPKOFNHVKV'
 
 
 class Solution:
-    def __init__(self, input_file):
-        self.input_file = input_file
-
     #
     # Solutions
     #
     @staticmethod
     def first():
         solution = Solution(INPUT_FILE)
+        print(solution.insertion_rules)
+
+        steps = 10
+        polymer = POLYMER_TEMPLATE
+
+        for step in range(steps):
+            polymer = solution.apply_insert_rules(polymer)
+            print(polymer)
+
+        element_counts = sorted(solution.count_elements(polymer))
+        print(element_counts)
+        lce = element_counts[0]
+        mce = element_counts[-1]
+        print(lce, mce)
+
+        return mce[0] - lce[0]
+
+    def count_elements(self, polymer):
+        counter = {}
+        for el in polymer:
+            counter[el] = counter.get(el, 0) + 1
+        return [(n, el) for el, n in counter.items()]
+
+    def apply_insert_rules(self, polymer):
+        print(polymer)
+        chain = []
+
+        for n, elem in enumerate(list(polymer)):
+            if n >= len(polymer) - 1:
+                break
+
+            next = polymer[n+1]
+            insert = self.insertion_rules[elem + next]
+            chain += [elem, insert, next]
+
+        print(chain)
+        raise 'die'
+        return ''.join(chain)
 
     @staticmethod
     def second():
@@ -35,10 +70,19 @@ class Solution:
             lines = file.readlines()
             return [line.strip() for line in lines]
 
+    @cached_property
+    def insertion_rules(self):
+        rules = {}
+        for line in self.input_lines:
+            pair, insert = line.split(' -> ')
+            rules[pair] = insert
+        return rules
+
     #
     # Methods
     #
-
+    def __init__(self, input_file):
+        self.input_file = input_file
 
 #
 # Main
