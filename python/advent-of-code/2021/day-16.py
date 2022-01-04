c0d5bd92fa7e08bef8c57e9d090d0d7a5c89281a
klenwell <klenwell@gmail.com>
2022-01-03 20:32:39 -0800

    Stubs out AOC 2021 Day 16.

---
+++ python/advent-of-code/2021/day-16.py
@@ -0,0 +1,50 @@
+"""
+Advent of Code 2021 - Day 16
+https://adventofcode.com/2021/day/16
+"""
+from os.path import join as path_join
+from functools import cached_property
+from config import INPUT_DIR
+
+
+INPUT_FILE = path_join(INPUT_DIR, 'day-16.txt')
+
+
+class Solution:
+    #
+    # Solutions
+    #
+    @staticmethod
+    def test():
+        pass
+
+    @staticmethod
+    def first():
+        pass
+
+    @staticmethod
+    def second():
+        pass
+
+    #
+    # Properties
+    #
+    @cached_property
+    def input_lines(self):
+        with open(self.input_file) as file:
+            lines = file.readlines()
+            return [line.strip() for line in lines]
+
+    #
+    # Methods
+    #
+    def __init__(self, input_file):
+        self.input_file = input_file
+
+
+#
+# Main
+#
+print("test: {}".format(Solution.test()))
+print("pt 1 solution: {}".format(Solution.first()))
+print("pt 2 solution: {}".format(Solution.second()))
