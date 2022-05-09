6ccf9ddbc67e97d5aef41023f9e78de97007dbf1
klenwell <klenwell@gmail.com>
2022-05-08 18:21:35 -0700

    Moves infinite iterator tests into new suite.

--- python/interview-topics/itertools_demo.py
+++ python/interview-topics/itertools_demo.py
@@ -9,74 +9,6 @@ from dataclasses import dataclass
 
 
 class IterToolsTest(unittest.TestCase):
-    def test_expects_to_count_to_five(self):
-        # Arrange
-        in_count_loop = itertools.count(start=1, step=1)
-        sequence = []
-
-        # Act
-        while len(sequence) < 5:
-            sequence.append(next(in_count_loop))
-
-        # Assert
-        self.assertEqual(sequence, [1, 2, 3, 4, 5])
-
-    def test_expects_to_count_by_twos(self):
-        # Arrange
-        in_count_loop = itertools.count(start=0, step=2)
-        sequence = []
-
-        # Act
-        while len(sequence) < 5:
-            sequence.append(next(in_count_loop))
-
-        # Assert
-        self.assertEqual(sequence, [0, 2, 4, 6, 8])
-
-    def test_expects_to_cycle_three_times(self):
-        # Arrange
-        in_cycle_loop = itertools.cycle('ABCD')
-        sequence = []
-
-        # Act
-        while sequence.count('D') < 3:
-            sequence.append(next(in_cycle_loop))
-
-        # Assert
-        self.assertEqual(sequence.count('A'), 3)
-        self.assertEqual(sequence.count('D'), 3)
-
-    def test_expects_to_repeat_endlessly(self):
-        # Arrange
-        in_repeat_loop = itertools.repeat('nom')
-        sequence = []
-
-        # Act
-        while len(sequence) < 100:
-            sequence.append(next(in_repeat_loop))
-
-        # Assert
-        self.assertEqual(len(sequence), 100)
-        self.assertEqual(sequence[0], 'nom')
-
-    def test_expects_to_repeat_three_times(self):
-        # Arrange
-        in_repeat_loop = itertools.repeat('nom', 3)
-        sequence = []
-        keep_repeating = True
-
-        # Act
-        while keep_repeating:
-            try:
-                sequence.append(next(in_repeat_loop))
-            except StopIteration:
-                keep_repeating = False
-
-        # Assert
-        self.assertEqual(len(sequence), 3)
-        self.assertEqual(sequence, ['nom'] * 3)
-        self.assertEqual(sequence, list(itertools.repeat('nom', 3)))
-
     def test_accumulate(self):
         accumulation = list(itertools.accumulate([1, 2, 3, 4]))
         self.assertEqual(accumulation, [1, 3, 6, 10])
@@ -191,6 +123,76 @@ class IterToolsTest(unittest.TestCase):
 
         self.assertEqual(zipped[0], ('A', 1, 'Alice'))
         self.assertEqual(zipped[-1], (None, 9, None))
+
+
+class IterToolsInfiniteIteratorsTest(unittest.TestCase):
+    def test_expects_to_count_to_five(self):
+        # Arrange
+        in_count_loop = itertools.count(start=1, step=1)
+        sequence = []
+
+        # Act
+        while len(sequence) < 5:
+            sequence.append(next(in_count_loop))
+
+        # Assert
+        self.assertEqual(sequence, [1, 2, 3, 4, 5])
+
+    def test_expects_to_count_by_twos(self):
+        # Arrange
+        in_count_loop = itertools.count(start=0, step=2)
+        sequence = []
+
+        # Act
+        while len(sequence) < 5:
+            sequence.append(next(in_count_loop))
+
+        # Assert
+        self.assertEqual(sequence, [0, 2, 4, 6, 8])
+
+    def test_expects_to_cycle_three_times(self):
+        # Arrange
+        in_cycle_loop = itertools.cycle('ABCD')
+        sequence = []
+
+        # Act
+        while sequence.count('D') < 3:
+            sequence.append(next(in_cycle_loop))
+
+        # Assert
+        self.assertEqual(sequence.count('A'), 3)
+        self.assertEqual(sequence.count('D'), 3)
+
+    def test_expects_to_repeat_endlessly(self):
+        # Arrange
+        in_repeat_loop = itertools.repeat('nom')
+        sequence = []
+
+        # Act
+        while len(sequence) < 100:
+            sequence.append(next(in_repeat_loop))
+
+        # Assert
+        self.assertEqual(len(sequence), 100)
+        self.assertEqual(sequence[0], 'nom')
+
+    def test_expects_to_repeat_three_times(self):
+        # Arrange
+        in_repeat_loop = itertools.repeat('nom', 3)
+        sequence = []
+        keep_repeating = True
+
+        # Act
+        while keep_repeating:
+            try:
+                sequence.append(next(in_repeat_loop))
+            except StopIteration:
+                keep_repeating = False
+
+        # Assert
+        self.assertEqual(len(sequence), 3)
+        self.assertEqual(sequence, ['nom'] * 3)
+        self.assertEqual(sequence, list(itertools.repeat('nom', 3)))
 
 
 #
