d90b6c846e3c0a21af897d98aff91169d6b4818e
klenwell <klenwell@gmail.com>
2022-05-10 13:24:33 -0700

    Simulates request to fetch data from meme API on button click.

--- react/codecamp-tutorial/react-meme-generator/src/components/MemeForm.js
+++ react/codecamp-tutorial/react-meme-generator/src/components/MemeForm.js
@@ -1,11 +1,31 @@
-import logoSrc from '../images/logo.png';
+import memesData from '../data/memes'
+
 
 export default function MemeForm() {
+  function handleClick(e) {
+    const memes = memesData['data']['memes']
+    const randomNum = randomInteger(0, memes.length)
+    const randomMeme = memes[randomNum]
+    console.log(randomMeme)
+  }
+
+  function handleMouseOver(e) {
+    console.log("Mouse over!")
+  }
+
+  function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   return (
-    <form className="meme-form">
+    <div className="meme-form">
       <input type="text" placeholder="Shut up" />
       <input type="text" placeholder="and take my money" />
-      <button>Get a new meme image</button>
-    </form>
+      <button
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}>
+        Get a new meme image
+      </button>
+    </div>
   )
 }
