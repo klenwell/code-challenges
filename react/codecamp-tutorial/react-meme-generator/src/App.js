c6ecd0019e9a0d3d5f23564b49479f7325149b5e
klenwell <klenwell@gmail.com>
2022-05-10 15:57:08 -0700

    Stubs out simple example using React state.

--- react/codecamp-tutorial/react-meme-generator/src/App.js
+++ react/codecamp-tutorial/react-meme-generator/src/App.js
@@ -1,13 +1,26 @@
 import './index.css'
+import React from 'react'
 import Header from './components/Header'
 import MemeForm from './components/MemeForm'
 
 export default function App() {
+  const [things, setThings] = React.useState(['thing 1', 'thing 2'])
+  const thingEls = things.map(thing => <p key={thing}>{thing}</p>)
+
+  function addItem() {
+    const idx = things.length + 1
+    const nextThing = `Thing ${idx}`
+    setThings(prevState => [...prevState, nextThing])
+    console.log(things)
+  }
+
   return (
     <div className="container">
       <Header />
       <main>
         <MemeForm />
+        <button onClick={addItem}>Add Item</button>
+        {thingEls}
       </main>
     </div>
   );
