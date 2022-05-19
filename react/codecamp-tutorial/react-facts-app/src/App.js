2ee50ea99402cf2ec78021180c08ecf56d68731a
klenwell <klenwell@gmail.com>
2022-05-19 09:58:57 -0700

    Stubs out csv table component.

--- react/codecamp-tutorial/react-facts-app/src/App.js
+++ react/codecamp-tutorial/react-facts-app/src/App.js
@@ -1,12 +1,30 @@
+import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar'
 import Main from './components/Main'
+import CsvTable from './components/CsvTable'
+
+
+const SHEET_ID = '1M7BfyPuwHQiavFtH59sgI9lJ7HjBpjXdBB-5BWv15K4'
+const API_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&sheet=Data`
+
 
 function App() {
+
+  const [ocCovidData, setocCovidData] = React.useState({})
+
+  function fetchCovidData() {
+    setocCovidData([])
+  }
+
+  React.useEffect(() => {
+  }, [])
+
   return (
     <div className="container">
       <Navbar />
       <Main />
+      <CsvTable />
     </div>
   )
 }
---
+++ react/codecamp-tutorial/react-facts-app/src/components/CsvTable.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+export default function CsvTable() {
+  return (
+    <div class="csv-table">
+      <table>
+        <thead>
+          <tr>
+            <th>Header</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Data</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
