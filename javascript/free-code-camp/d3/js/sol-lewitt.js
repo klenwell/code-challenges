0cdd92bc8206cce64008f27674422056f5276ab9
klenwell <klenwell@gmail.com>
2022-05-27 19:03:29 -0700

    Stubs out new SolLewittSvg class.

---
+++ javascript/free-code-camp/d3/js/sol-lewitt.js
@@ -0,0 +1,27 @@
+/*
+ * SolLewittSvg
+ *
+ * For course: https://www.freecodecamp.org/news/data-visualizatoin-with-d3/
+ *
+ * Uses JS Class template:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+ */
+
+ class SolLewittSvg {
+  constructor() {
+  }
+
+  /*
+   * Getters
+  **/
+  get foo() {
+    return 'foo'
+  }
+
+  /*
+   * Methods
+  **/
+  bar() {
+    return 'bar'
+  }
+}
