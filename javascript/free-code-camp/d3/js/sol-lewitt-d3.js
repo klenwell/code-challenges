ed62e557cafd803744513aa2452dc39f78f5a3f8
klenwell <klenwell@gmail.com>
2022-06-03 13:51:06 -0700

    Reverse pattern on LeWitt circle for lesson 4.

--- javascript/free-code-camp/d3/js/sol-lewitt-d3.js
+++ javascript/free-code-camp/d3/js/sol-lewitt-d3.js
@@ -18,7 +18,7 @@ class SolLewittD3 {
   /*
    * Static Methods
   **/
-  static dev(parentId) {
+  static circle(parentId) {
     const canvasWidth = 400
     const canvasHeight = 400
     const parent = d3.select(`#${parentId}`)
@@ -29,21 +29,22 @@ class SolLewittD3 {
       .attr('width', canvasWidth)
       .attr('height', canvasHeight)
 
-    // Create SVG background: rows
-    const rowHeight = 10
-    const numRows = canvasHeight / rowHeight
+    // Create SVG background: columns
+    const colWidth = 10
+    const numCols = canvasWidth / colWidth
     svg.selectAll('rect')
-      .data(d3.range(numRows))
+      .data(d3.range(numCols))
       .join('rect')
-      .attr('y', (n) => n * 10)
-      .attr('width', canvasWidth)
-      .attr('height', rowHeight)
+      .attr('x', (n) => n * 10)
+      .attr('y', 0)
+      .attr('width', colWidth)
+      .attr('height', canvasHeight)
       .attr('fill', (n) => n % 2 == 0 ? 'black' : 'white')
 
     // Create SVG foreground: columns in circle
     const circleRadius = canvasHeight / 3
-    const colWidth = 10
-    const numCols = canvasWidth / colWidth
+    const rowHeight = 10
+    const numRows = canvasHeight / rowHeight
     const posLeft = 0
     const posTop = (canvasHeight / 2) - circleRadius
     const styleAttr = `position: absolute; left: ${posLeft}px; top: ${posTop}px`
@@ -54,12 +55,11 @@ class SolLewittD3 {
       .attr('style', styleAttr)
 
     fgSvg.selectAll('rect')
-      .data(d3.range(numCols))
+      .data(d3.range(numRows))
       .join('rect')
-      .attr('x', (n) => n * 10)
-      .attr('y', 0)
-      .attr('width', colWidth)
-      .attr('height', canvasHeight)
+      .attr('y', (n) => n * 10)
+      .attr('width', canvasWidth)
+      .attr('height', rowHeight)
       .attr('fill', (n) => n % 2 == 0 ? 'black' : 'white')
 
     // Create column mask: cut out columns in circle
@@ -79,71 +79,6 @@ class SolLewittD3 {
     fgSvg.attr('mask', `url(#${colMaskId})`)
   }
 
-  static reproduce(parentId) {
-    const canvasWidth = 400
-    const canvasHeight = 400
-    const parent = document.getElementById(parentId)
-
-    // Create SVG and append to parent element
-    const sol = new SolLewittD3()
-
-    // Create background SVG: rows
-    const bgSvg = sol.createSvgEl('svg', {width: canvasWidth, height: canvasHeight})
-
-    const rowHeight = 10
-    const numRows = canvasHeight / rowHeight
-    for (let i = 0; i < numRows; i++) {
-      let fillColor = i % 2 == 0 ? 'black' : 'white'
-      let y = i * 10
-      let row = sol.createRect(canvasWidth, rowHeight, 0, y, {fill: fillColor})
-      bgSvg.appendChild(row)
-    }
-
-    parent.appendChild(bgSvg)
-
-    // Create foreground SVG: columns in circle
-    const circleRadius = canvasHeight / 3
-    const fgSvg = sol.createSvgEl('svg', {
-      width: canvasWidth,
-      height: canvasHeight
-    })
-    fgSvg.style.position = 'absolute'
-    fgSvg.style.top = `${(canvasHeight / 2) - circleRadius}px`
-    fgSvg.style.left = '0px'
-
-    const colWidth = 10
-    const numCols = canvasWidth / colWidth
-    for (let i = 0; i < numCols; i++) {
-      let fillColor = i % 2 == 0 ? 'black' : 'white'
-      let x = i * 10
-      let col = sol.createRect(colWidth, canvasHeight, x, 0, {fill: fillColor})
-      fgSvg.appendChild(col)
-    }
-
-    parent.appendChild(fgSvg)
-
-    // Create column mask: cut out columns in circle
-    const colMask = sol.createSvgEl('mask')
-    colMask.setAttribute('id', 'col-mask')
-    fgSvg.appendChild(colMask)
-
-    const colMaskRect = sol.createSvgEl('rect')
-    colMaskRect.setAttribute('width', canvasWidth)
-    colMaskRect.setAttribute('height', canvasHeight)
-    colMaskRect.setAttribute('fill', 'black')
-    colMask.append(colMaskRect)
-
-    const colMaskCircle = sol.createCircle(
-      circleRadius,
-      canvasWidth / 2,
-      canvasHeight / 2,
-      {fill: 'white'}
-    )
-    colMask.append(colMaskCircle)
-
-    fgSvg.setAttribute('mask', 'url(#col-mask)')
-  }
-
   /*
    * Getters
   **/
@@ -151,41 +86,4 @@ class SolLewittD3 {
   /*
    * Methods
   **/
-  createSvgEl(el, attrs) {
-    attrs = attrs !== undefined ? attrs : {}
-    const svg = document.createElementNS(SVG_NS_URL, el)
-    for (const [attr, value] of Object.entries(attrs)) {
-      svg.setAttribute(attr, value)
-    }
-    return svg
-  }
-
-  createRect(width, height, x, y, attrs) {
-    const rectAttrs = {
-      width: width,
-      height: height,
-      x: x,
-      y: y
-    }
-    const elAttrs = Object.assign(rectAttrs, attrs)
-    return this.createSvgEl('rect', elAttrs)
-  }
-
-  createCircle(r, cx, cy, attrs) {
-    const rectAttrs = {
-      r: r,
-      cx: cx,
-      cy: cy
-    }
-    const elAttrs = Object.assign(rectAttrs, attrs)
-    return this.createSvgEl('circle', elAttrs)
-  }
-
-  createMask(el, id) {
-    const attrs = {
-      id: id,
-      'data-url': `url(#${id})`
-    }
-    return this.createSvgEl('mask', attrs)
-  }
 }
