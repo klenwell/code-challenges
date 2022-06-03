b31c67758a4eb27bd69723d2525185e01a209c90
klenwell <klenwell@gmail.com>
2022-06-03 12:08:11 -0700

    Reproduces Sol LeWitt circle with D3. Noticed pattern is inverse of expected.

--- javascript/free-code-camp/d3/js/sol-lewitt-d3.js
+++ javascript/free-code-camp/d3/js/sol-lewitt-d3.js
@@ -21,32 +21,62 @@ class SolLewittD3 {
   static dev(parentId) {
     const canvasWidth = 400
     const canvasHeight = 400
+    const parent = d3.select(`#${parentId}`)
 
-    const svg = d3.select(`#${parentId}`)
+    // Create SVG
+    const svg = parent
       .append('svg')
       .attr('width', canvasWidth)
       .attr('height', canvasHeight)
 
-      const rowHeight = 10
-      const numRows = canvasHeight / rowHeight
-      const marks = []
-      for (let i = 0; i < numRows; i++) {
-        marks.push({
-          y: i * 10,
-          width: canvasWidth,
-          height: rowHeight,
-          fill: i % 2 == 0 ? 'black' : 'white'
-        })
-      }
+    // Create SVG background: rows
+    const rowHeight = 10
+    const numRows = canvasHeight / rowHeight
+    svg.selectAll('rect')
+      .data(d3.range(numRows))
+      .join('rect')
+      .attr('y', (n) => n * 10)
+      .attr('width', canvasWidth)
+      .attr('height', rowHeight)
+      .attr('fill', (n) => n % 2 == 0 ? 'black' : 'white')
 
-      svg
-        .selectAll('rect')
-        .data(marks)
-        .join('rect')
-        .attr('y', (d) => d.y)
-        .attr('width', (d) => d.width)
-        .attr('height', (d) => d.height)
-        .attr('fill', (d) => d.fill)
+    // Create SVG foreground: columns in circle
+    const circleRadius = canvasHeight / 3
+    const colWidth = 10
+    const numCols = canvasWidth / colWidth
+    const posLeft = 0
+    const posTop = (canvasHeight / 2) - circleRadius
+    const styleAttr = `position: absolute; left: ${posLeft}px; top: ${posTop}px`
+    const fgSvg = parent
+      .append('svg')
+      .attr('width', canvasWidth)
+      .attr('height', canvasHeight)
+      .attr('style', styleAttr)
+
+    fgSvg.selectAll('rect')
+      .data(d3.range(numCols))
+      .join('rect')
+      .attr('x', (n) => n * 10)
+      .attr('y', 0)
+      .attr('width', colWidth)
+      .attr('height', canvasHeight)
+      .attr('fill', (n) => n % 2 == 0 ? 'black' : 'white')
+
+    // Create column mask: cut out columns in circle
+    const colMaskId = 'col-mask'
+    const colMask = fgSvg.append('mask').attr('id', colMaskId)
+    const colMaskRect = colMask.append('rect')
+      .attr('width', canvasWidth)
+      .attr('height', canvasHeight)
+      .attr('fill', 'black')
+    const colMaskCircle = colMask.append('circle')
+      .attr('r', circleRadius)
+      .attr('cx', canvasWidth / 2)
+      .attr('cy', canvasHeight / 2)
+      .attr('fill', 'white')
+
+    // Apply mask
+    fgSvg.attr('mask', `url(#${colMaskId})`)
   }
 
   static reproduce(parentId) {
