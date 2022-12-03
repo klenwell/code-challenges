7ca4a63100ed0e7b1349d975f3fe0fb75148c7d1
klenwell <klenwell@gmail.com>
2022-12-02 21:40:20 -0800

    Cleans up AoC Day 3.

--- python/advent-of-code/2022/day-03.py
+++ python/advent-of-code/2022/day-03.py
@@ -1,10 +1,15 @@
 """
 Advent of Code 2022 - Day 3
 https://adventofcode.com/2022/day/3
+
+References:
+https://www.pythontutorial.net/python-basics/python-set-intersection/
+
 """
 from os.path import join as path_join
 from functools import cached_property
 from config import INPUT_DIR
+import string
 
 
 INPUT_FILE = path_join(INPUT_DIR, 'day-03.txt')
@@ -21,43 +26,24 @@ class Solution:
     def first(self):
         sum = 0
         for line in self.input_lines:
-            ruck1, ruck2 = self.split_sack(line)
-            common_item = list(set(ruck1).intersection(ruck2))[0]
-            print(line, ruck1, ruck2, common_item)
+            part1, part2 = self.split_sack(line)
+            common_item = list(set(part1).intersection(part2))[0]
             value = self.prioritize(common_item)
-            print(common_item, value)
             sum += value
         return sum
-
-    def split_sack(self, line):
-        line_len = len(line)
-        half = int(line_len / 2)
-        return line[:half], line[half:]
-
-    def prioritize(self, item):
-        import string
-        priority_map = list(string.ascii_lowercase)
-
-        idx = item.lower()
-        value = priority_map.index(idx) + 1
-
-        if item.isupper():
-            value += 26
-        return value
-
 
     @property
     def second(self):
         sum = 0
         step = 3
 
+        # https://www.geeksforgeeks.org/break-list-chunks-size-n-python/
+        # Isn't there an itertool that does this?
         for i in range(0, len(self.input_lines), step):
             x = i
             ruck1, ruck2, ruck3 = self.input_lines[x:x+step]
             badge = list(set(ruck1) & set(ruck2) & set(ruck3))[0]
-            print(ruck1, ruck2, badge)
             value = self.prioritize(badge)
-            print(badge, value)
             sum += value
 
         return sum
@@ -74,6 +60,22 @@ class Solution:
     #
     # Methods
     #
+    def split_sack(self, line):
+        line_len = len(line)
+        half = int(line_len / 2)
+        return line[:half], line[half:]
+
+    def prioritize(self, item):
+        # https://stackoverflow.com/a/3190207/1093087
+        priorities = list(string.ascii_lowercase)
+
+        idx = item.lower()
+        value = priorities.index(idx) + 1
+
+        if item.isupper():
+            value += 26
+
+        return value
 
 
 #
