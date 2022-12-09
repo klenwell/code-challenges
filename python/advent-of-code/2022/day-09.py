b18b8021a1f356e851df7e7c14e09a2776ceeca8
klenwell <klenwell@gmail.com>
2022-12-08 22:55:10 -0800

    Solves AoC Day 9 part 2.

--- python/advent-of-code/2022/day-09.py
+++ python/advent-of-code/2022/day-09.py
@@ -105,6 +105,83 @@ class Rope:
             self.move_tail('R') if dx > 0 else self.move_tail('L')
 
 
+class KnottyRope:
+    def __init__(self, knots):
+        self.knots = [[0,0] for knot in range(knots)]
+        self.tail_pts = []
+
+    def simulate(self, motions):
+        for motion in motions:
+            dir, steps = motion.split(' ')
+            for n in range(int(steps)):
+                self.move_head(dir)
+                print(motion, n, self.knots)
+
+    def move_head(self, dir):
+        self.move_knot(self.knots[0], dir)
+
+        for n, knot in enumerate(self.knots):
+            if n == 0:
+                continue
+            knot_head = self.knots[n-1]
+            if not self.knot_is_touching_head(knot, knot_head):
+                self.pull_knot(knot_head, knot)
+
+        self.tail_pts.append(tuple(self.knots[-1]))
+
+    def knot_is_touching_head(self, knot, knot_head):
+        dx, dy = self.diff_head_knot(knot_head, knot)
+        return abs(dx) < 2 and abs(dy) < 2
+
+    def pull_knot(self, knot_head, knot):
+        dx, dy = self.diff_head_knot(knot_head, knot)
+
+        if dx == 0 or dy == 0:
+            self.move_knot_directionally(knot, dx, dy)
+        else:
+            self.move_knot_diagnolly(knot, dx, dy)
+
+    def diff_head_knot(self, knot_head, knot):
+        dx = knot_head[0] - knot[0]
+        dy = knot_head[1] - knot[1]
+        return (dx, dy)
+
+    def move_knot_directionally(self, knot, dx, dy):
+        if dx > 1:
+            self.move_knot(knot, 'R')
+        elif dx < -1:
+            self.move_knot(knot, 'L')
+
+        if dy > 1:
+            self.move_knot(knot, 'U')
+        elif dy < -1:
+            self.move_knot(knot, 'D')
+
+    def move_knot_diagnolly(self, knot, dx, dy):
+        if dx > 1:
+            self.move_knot(knot, 'R')
+            self.move_knot(knot, 'U') if dy > 0 else self.move_knot(knot, 'D')
+        elif dx < -1:
+            self.move_knot(knot, 'L')
+            self.move_knot(knot, 'U') if dy > 0 else self.move_knot(knot, 'D')
+        elif dy > 1:
+            self.move_knot(knot, 'U')
+            self.move_knot(knot, 'R') if dx > 0 else self.move_knot(knot, 'L')
+        elif dy < -1:
+            self.move_knot(knot, 'D')
+            self.move_knot(knot, 'R') if dx > 0 else self.move_knot(knot, 'L')
+
+    def move_knot(self, knot, dir):
+        if dir == 'R':
+            knot[0] += 1
+        elif dir == 'L':
+            knot[0] -= 1
+        elif dir == 'U':
+            knot[1] += 1
+        elif dir == 'D':
+            knot[1] -= 1
+
+
 class Solution:
     def __init__(self, input_file):
         self.input_file = input_file
@@ -117,12 +194,28 @@ class Solution:
         motions = self.test_input_lines
         rope = Rope()
         rope.simulate(motions)
-        print(rope.tail_pts)
         return len(set(rope.tail_pts))
 
     @property
     def test2(self):
-        pass
+        expected = 36
+        INPUT = """\
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
+
+        motions = [line.strip() for line in INPUT.split("\n")]
+        rope = KnottyRope(10)
+        rope.simulate(motions)
+        tail_pts = len(set(rope.tail_pts))
+        print(set(rope.tail_pts))
+        return tail_pts, tail_pts == expected, expected
+
 
     @property
     def first(self):
@@ -134,7 +227,10 @@ class Solution:
 
     @property
     def second(self):
-        pass
+        motions = self.input_lines
+        rope = KnottyRope(10)
+        rope.simulate(motions)
+        return len(set(rope.tail_pts))
 
     #
     # Properties
@@ -160,5 +256,5 @@ class Solution:
 solution = Solution(INPUT_FILE)
 print("test 1 solution: {}".format(solution.test1))
 print("test 2 solution: {}".format(solution.test2))
-print("pt 1 solution: {}".format(solution.first))
+#print("pt 1 solution: {}".format(solution.first))
 print("pt 2 solution: {}".format(solution.second))
