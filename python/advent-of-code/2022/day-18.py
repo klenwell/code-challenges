793b9551db93d87208fc4c82481b184e5501c0be
klenwell <klenwell@gmail.com>
2022-12-18 16:10:08 -0800

    Missed day 18 pt 2 (972).

--- python/advent-of-code/2022/day-18.py
+++ python/advent-of-code/2022/day-18.py
@@ -24,6 +24,7 @@ TEST_INPUT = """\
 2,1,5
 2,3,5"""
 
+
 class Cube:
     def __init__(self, x, y, z):
         self.x = int(x)
@@ -44,6 +45,7 @@ class Cube:
             (self.x, self.y, self.z-1),
             (self.x, self.y, self.z+1)
         ]
+
 
 class Solution:
     def __init__(self, input_file):
@@ -82,11 +84,84 @@ class Solution:
 
     @property
     def test2(self):
-        pass
+        surface_area = 0
+        filled_pts = set()
+        all_pts = set()
+        for line in self.test_input_lines:
+            surface_area += 6
+            x, y, z = [int(n) for n in line.split(',')]
+            cube = Cube(x, y, z)
+            filled_pts.add(cube.pt)
+            for pt in cube.neighbors:
+                all_pts.add(pt)
+                if pt in filled_pts:
+                    surface_area -= 2
+
+        empty_pts = all_pts - filled_pts
+
+        min_x = min([p[0] for p in filled_pts])
+        max_x = max([p[0] for p in filled_pts])
+        min_y = min([p[1] for p in filled_pts])
+        max_y = max([p[1] for p in filled_pts])
+        min_z = min([p[2] for p in filled_pts])
+        max_z = max([p[2] for p in filled_pts])
+
+        inner_empty_pts = set()
+        for x, y, z in empty_pts:
+            inner_x = x > min_x and x < max_x
+            inner_y = y > min_y and y < max_y
+            inner_z = z > min_z and z < max_z
+            if inner_x and inner_y and inner_z:
+                inner_empty_pts.add((x, y, z))
+
+        print(inner_empty_pts)
+
+        return surface_area - (len(inner_empty_pts) * 6)
 
     @property
     def second(self):
-        pass
+        surface_area = 0
+        filled_pts = set()
+        all_pts = set()
+        for line in self.input_lines:
+            surface_area += 6
+            x, y, z = [int(n) for n in line.split(',')]
+            cube = Cube(x, y, z)
+            filled_pts.add(cube.pt)
+            for pt in cube.neighbors:
+                all_pts.add(pt)
+                if pt in filled_pts:
+                    surface_area -= 2
+
+        empty_pts = all_pts - filled_pts
+
+        min_x = min([p[0] for p in filled_pts])
+        max_x = max([p[0] for p in filled_pts])
+        min_y = min([p[1] for p in filled_pts])
+        max_y = max([p[1] for p in filled_pts])
+        min_z = min([p[2] for p in filled_pts])
+        max_z = max([p[2] for p in filled_pts])
+
+        inner_empty_pts = set()
+        for x, y, z in empty_pts:
+            inner_x = x > min_x and x < max_x
+            inner_y = y > min_y and y < max_y
+            inner_z = z > min_z and z < max_z
+            if inner_x and inner_y and inner_z:
+                inner_empty_pts.add((x, y, z))
+
+        print(len(filled_pts), len(inner_empty_pts), surface_area)
+
+        # Subtract inner area pts
+        for x, y, z in filled_pts:
+            cube = Cube(x, y, z)
+            for pt in cube.neighbors:
+                if pt in inner_empty_pts:
+                    print(cube.pt, pt)
+                    surface_area -= 2
+
+        return surface_area
+        # 972 (too low)
 
     #
     # Properties
