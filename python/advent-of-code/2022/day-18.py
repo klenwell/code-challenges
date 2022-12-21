004e5e6a60514769227af14b7ca96e24dd8894d7
klenwell <klenwell@gmail.com>
2022-12-21 07:34:00 -0800

    Finally. I solved day 18 part 2.

--- python/advent-of-code/2022/day-18.py
+++ python/advent-of-code/2022/day-18.py
@@ -76,33 +76,9 @@ class LavaDroplet:
             for pt in cube.neighbors:
                 if pt in pts:
                     area -= 2
-                elif pt in self.air_cube_pts:
+                elif pt in self.trapped_interior_pts:
                     area -= 1
         return area
-
-    @cached_property
-    def min_x(self):
-        return min([c.x for c in self.cubes])
-
-    @cached_property
-    def max_x(self):
-        return max([c.x for c in self.cubes])
-
-    @cached_property
-    def min_y(self):
-        return min([c.y for c in self.cubes])
-
-    @cached_property
-    def max_y(self):
-        return max([c.y for c in self.cubes])
-
-    @cached_property
-    def min_z(self):
-        return min([c.z for c in self.cubes])
-
-    @cached_property
-    def max_z(self):
-        return max([c.z for c in self.cubes])
 
     @cached_property
     def cubes(self):
@@ -118,15 +94,60 @@ class LavaDroplet:
         return [c.pt for c in self.cubes]
 
     @cached_property
-    def air_cube_pts(self):
-        cube_pts = []
+    def trapped_interior_pts(self):
+        return self.interior_air_pts - self.exterior_pts
+
+    @cached_property
+    def interior_air_pts(self):
+        """These unfilled pts are within the perimeter of the droplet but may still be
+        accessible to the surface. Think Death Star's exhaust port.
+        """
+        cube_pts = set()
         for x in range(self.min_x, self.max_x + 1):
             for y in range(self.min_y, self.max_y + 1):
                 for z in range(self.min_z, self.max_z + 1):
                     pt = (x, y, z)
                     if pt not in self.cube_pts and self.is_interior_pt(pt):
-                        cube_pts.append(pt)
+                        cube_pts.add(pt)
         return cube_pts
+
+    @cached_property
+    def exterior_pts(self):
+        """Start at 0,0,0 and use BFS to search out all exposed empty external cube pts.
+        """
+        empty_pts = set()
+        visited_pts = set()
+        start_pt = (self.min_x, self.min_y, self.min_x)
+        pts_to_visit = [start_pt]
+        n = 0
+
+        while pts_to_visit:
+            n += 1
+            pt = pts_to_visit.pop(0)
+            visited_pts.add(pt)
+
+            # if n % 1000 == 0:
+            #     print(n, pt, len(pts_to_visit), len(set(pts_to_visit)), len(visited_pts), len(empty_pts))
+
+            if pt not in self.cube_pts:
+                empty_pts.add(pt)
+
+            cube = Cube(*pt)
+            for new_pt in cube.neighbors:
+                if new_pt in visited_pts:
+                    continue
+                if new_pt in self.cube_pts:
+                    continue
+                if new_pt[0] < self.min_x or new_pt[0] > self.max_x:
+                    continue
+                if new_pt[1] < self.min_y or new_pt[1] > self.max_y:
+                    continue
+                if new_pt[2] < self.min_z or new_pt[2] > self.max_z:
+                    continue
+                if new_pt not in pts_to_visit:
+                    pts_to_visit.append(new_pt)
+
+        return empty_pts
 
     def is_interior_pt(self, pt):
         (x, y, z) = pt
@@ -182,6 +203,30 @@ class LavaDroplet:
                 xy_slices[xy] = [z]
         return xy_slices
 
+    @cached_property
+    def min_x(self):
+        return min([c.x for c in self.cubes])
+
+    @cached_property
+    def max_x(self):
+        return max([c.x for c in self.cubes])
+
+    @cached_property
+    def min_y(self):
+        return min([c.y for c in self.cubes])
+
+    @cached_property
+    def max_y(self):
+        return max([c.y for c in self.cubes])
+
+    @cached_property
+    def min_z(self):
+        return min([c.z for c in self.cubes])
+
+    @cached_property
+    def max_z(self):
+        return max([c.z for c in self.cubes])
+
 
 class Solution:
     def __init__(self, input_file):
@@ -212,12 +257,14 @@ class Solution:
     @property
     def test2(self):
         droplet = LavaDroplet(self.test_input_lines)
-        assert droplet.air_cube_pts == [(2, 2, 5)]
+        print(droplet.interior_air_pts, droplet.trapped_interior_pts)
+        assert droplet.trapped_interior_pts == set([(2, 2, 5)])
         return droplet.exterior_surface_area
 
     @property
     def second(self):
         droplet = LavaDroplet(self.input_lines)
+        print(len(droplet.interior_air_pts), len(droplet.trapped_interior_pts))
         return droplet.exterior_surface_area
         # 972 (too low)
         # 2470 (too low)
@@ -237,10 +284,6 @@ class Solution:
     @cached_property
     def test_input_lines(self):
         return [line.strip() for line in TEST_INPUT.split("\n")]
-
-    #
-    # Methods
-    #
 
 
 #
