4dcbf3fa117c09191a881ad956141e4f0c38d809
klenwell <klenwell@gmail.com>
2023-01-29 09:47:45 -0800

    Stubs out AoC 2015 Day 7.

--- python/advent-of-code/2015/day-06.py
+++ python/advent-of-code/2015/day-06.py
@@ -1,6 +1,8 @@
 """
 Advent of Code 2015 - Day 6
 https://adventofcode.com/2015/day/6
+
+Day 6: Probably a Fire Hazard
 """
 from os.path import join as path_join
 from functools import cached_property
