b58d1f46cb7b75a0993cd1bcc1a30b068a69fc2e
klenwell <klenwell@gmail.com>
2023-01-28 16:02:37 -0800

    Stubs out AoC 2015 Day 6.

--- python/advent-of-code/2015/day-05.py
+++ python/advent-of-code/2015/day-05.py
@@ -1,6 +1,8 @@
 """
 Advent of Code 2015 - Day 5
 https://adventofcode.com/2022/day/5
+
+Day 5: Doesn't He Have Intern-Elves For This?
 """
 from os.path import join as path_join
 from functools import cached_property
