42800e4556dbf643b442428a9f0d481ef6ab1c3b
klenwell <klenwell@gmail.com>
2023-02-04 15:13:35 -0800

    Stubs out AoC 2015 Day 11 puzzle.

--- python/advent-of-code/2015/common.py
+++ python/advent-of-code/2015/common.py
@@ -8,6 +8,7 @@ To profile:
 $ python -m cProfile -s cumtime 2015/day-00.py
 """
 from os.path import dirname, join as path_join
+import time
 
 
 # Common Paths
@@ -19,8 +20,14 @@ INPUT_DIR = path_join(ROOT_DIR, 'inputs')
 def info(msg, freq):
     # https://stackoverflow.com/q/279561/1093087
     try:
+        ts = time.time()
+        info.split_time = ts - info.last_ts
+    except AttributeError:
+        info.split_time = 0
+    try:
         info.counter += 1
     except AttributeError:
         info.counter = 0
     if info.counter % freq == 0:
-        print(f"[info:{info.counter}] {msg}")
+        info.last_ts = ts
+        print(f"[info:{info.counter}] ({info.split_time:.3f}) {msg}")
