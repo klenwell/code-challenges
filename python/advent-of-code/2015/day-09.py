11cf3cd5491e62ebfe89d792d70e30ec868dd942
klenwell <klenwell@gmail.com>
2023-02-03 18:59:19 -0800

    Stubs out AoC 2015 Day 10.

--- python/advent-of-code/2015/day-09.py
+++ python/advent-of-code/2015/day-09.py
@@ -1,6 +1,8 @@
 """
 Advent of Code 2015 - Day 09
 https://adventofcode.com/2015/day/9
+
+Day 9: All in a Single Night
 """
 from os.path import join as path_join
 from functools import cached_property
