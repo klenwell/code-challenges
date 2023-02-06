5c58341e0fad5abf4fd90188ed20fe48ac9beec8
klenwell <klenwell@gmail.com>
2023-02-05 17:48:11 -0800

    Stubs out AoC 2015 Day 12.

---
+++ python/advent-of-code/2015/day-12.py
@@ -0,0 +1,64 @@
+"""
+Advent of Code 2015 - Day 12
+https://adventofcode.com/2015/day/12
+
+Day 12: JSAbacusFramework.io
+"""
+from os.path import join as path_join
+from functools import cached_property
+from common import INPUT_DIR
+
+
+class DailyPuzzle:
+    INPUT_FILE = path_join(INPUT_DIR, 'day-12.txt')
+
+    TEST_INPUT = """\
+"""
+
+    def solve(self):
+        print(f"test 1 solution: {self.test1}")
+        print(f"Part 1 Solution: {self.first}")
+        print(f"test 2 solution: {self.test2}")
+        print(f"Part 2 Solution: {self.second}")
+
+    #
+    # Solutions
+    #
+    @property
+    def first(self):
+        input = self.file_input
+        return input
+
+    @property
+    def second(self):
+        pass
+
+    #
+    # Tests
+    #
+    @property
+    def test1(self):
+        input = self.TEST_INPUT
+        print(input)
+        return 'passed'
+
+    @property
+    def test2(self):
+        input = self.TEST_INPUT
+        print(input)
+        return 'passed'
+
+    #
+    # Properties
+    #
+    @cached_property
+    def file_input(self):
+        with open(self.INPUT_FILE) as file:
+            return file.read().strip()
+
+
+#
+# Main
+#
+problem = DailyPuzzle()
+problem.solve()
