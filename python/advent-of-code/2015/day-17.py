b2582484707c7428c6b542985f9e402bbf016d14
klenwell <klenwell@gmail.com>
2023-02-11 12:53:12 -0800

    Day 17 Test 1 passes.

--- python/advent-of-code/2015/day-17.py
+++ python/advent-of-code/2015/day-17.py
@@ -7,11 +7,29 @@ from functools import cached_property
 from common import INPUT_DIR
 
 
+def subset_sum(array, num):
+    result = []
+    def find(arr, num, path=()):
+        if not arr:
+            return
+        if arr[0] == num:
+            result.append(path + (arr[0],))
+        else:
+            find(arr[1:], num - arr[0], path + (arr[0],))
+        find(arr[1:], num, path)
+    find(array, num)
+    return result
+
+
 class AdventPuzzle:
     INPUT_FILE = path_join(INPUT_DIR, 'day-17.txt')
 
     TEST_INPUT = """\
-"""
+20
+15
+10
+5
+5"""
 
     def solve(self):
         print(f"test 1 solution: {self.test1}")
@@ -36,8 +54,11 @@ class AdventPuzzle:
     #
     @property
     def test1(self):
+        target = 25
         input = self.TEST_INPUT
-        print(input)
+        array = [int(n) for n in input.split('\n')]
+        subsets = subset_sum(array, target)
+        assert len(subsets) == 4, subsets
         return 'passed'
 
     @property
