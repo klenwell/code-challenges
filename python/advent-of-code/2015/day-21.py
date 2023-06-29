7dbb5498a708f76c428b117e7194af40d09a8e0f
klenwell <klenwell@gmail.com>
2023-06-28 21:31:36 -0700

    2015 Day 21 Test 1 (short battle) passes.

--- python/advent-of-code/2015/day-21.py
+++ python/advent-of-code/2015/day-21.py
@@ -7,6 +7,40 @@ from functools import cached_property
 from common import INPUT_DIR
 
 
+class Character:
+    def __init__(self, hp, damage, armor):
+        self.hp = hp
+        self.damage = damage
+        self.armor = armor
+
+    def attacks(self, defender):
+        if self.is_dead():
+            raise DeadCharacter(self)
+        damage = self.damage - defender.armor
+        damage = max(damage, 1)
+        defender.hp = defender.hp - damage
+        return defender
+
+    def is_dead(self):
+        return self.hp <= 0
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} hp={self.hp} damage={self.damage} armor={self.armor}>"
+
+
+class Boss(Character): pass
+
+
+class Player(Character):
+    def __init__(self, hp, damage, armor):
+        super().__init__(hp, damage, armor)
+        self.spent = 0
+
+
+class DeadCharacter(Exception): pass
+
+
+
 class AdventPuzzle:
     INPUT_FILE = path_join(INPUT_DIR, 'day-21.txt')
 
@@ -36,8 +70,24 @@ class AdventPuzzle:
     #
     @property
     def test1(self):
-        input = self.TEST_INPUT
-        print(input)
+        battle_over = False
+        rounds = 0
+        player = Player(8, 5, 5)
+        boss = Boss(12, 7, 2)
+
+        while not battle_over:
+            rounds += 1
+            print(f"Round: {rounds}")
+            try:
+                player.attacks(boss)
+                boss.attacks(player)
+                print(player, boss)
+            except DeadCharacter as loser:
+                print(f"Loser: {loser}")
+                battle_over = True
+
+        assert boss.is_dead(), boss
+        assert player.hp == 2, player.hp
         return 'passed'
 
     @property
