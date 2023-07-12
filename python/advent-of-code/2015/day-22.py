cdef601d5e24bc4aac6cf52bed1ea5be74625f9c
klenwell <klenwell@gmail.com>
2023-07-11 21:37:57 -0700

    Found error in battle model. Still have not solved part 1.

--- python/advent-of-code/2015/day-22.py
+++ python/advent-of-code/2015/day-22.py
@@ -11,11 +11,11 @@ from common import INPUT_DIR, info
 
 SPELLS = [
     # Name, Cost, Duration, Damage, Defense, Heal, Recharge
-    ('Magic Missle', 53, 1, 4, 0, 0, 0),
-    ('Drain',        73, 1, 2, 0, 2, 0),
-    ('Shield',      113, 6, 0, 7, 0, 0),
-    ('Poison',      173, 6, 3, 0, 0, 0),
-    ('Recharge',    229, 5, 0, 0, 0, 101)
+    ('Magic Missile', 53, 0, 4, 0, 0, 0),
+    ('Drain',         73, 0, 2, 0, 2, 0),
+    ('Shield',       113, 6, 0, 7, 0, 0),
+    ('Poison',       173, 6, 3, 0, 0, 0),
+    ('Recharge',     229, 5, 0, 0, 0, 101)
 ]
 
 
@@ -63,7 +63,6 @@ class Wizard:
         queue = [self]
 
         while queue:
-            #info([w.foe.hp for w in queue], 1000)
             wizard = queue.pop()
             clones = wizard.branch_clones(winner)
 
@@ -75,9 +74,6 @@ class Wizard:
                         winner = clone
                     elif clone.mana_spent < winner.mana_spent:
                         winner = clone
-                    queue = [c for c in queue if c.mana_spent < winner.mana_spent]
-                elif winner and clone.mana_spent < winner.mana_spent:
-                    queue.append(clone)
                 else:
                     queue.append(clone)
 
@@ -90,51 +86,71 @@ class Wizard:
     def branch_clones(self, winner):
         clones = []
         for spell in self.spell_options:
-            if winner and winner.mana_spent <= self.mana_spent + spell.cost:
-                continue
+            # if winner and winner.mana_spent <= self.mana_spent + spell.cost:
+            #     continue
             clone = self.clone()
             clone.battle_round(spell)
             clones.append(clone)
         return clones
 
     def battle_round(self, spell):
-        self.cast_spell(spell)
+        # Effects all work the same way. Effects apply at the start of both the
+        # player's turns and the boss' turns.
+        # Player's Turn
         self.apply_spell_effects(self.foe)
-        self.foe.attacks(self)
+        self.cast_spell(spell, self.foe)
+
+        # Boss's Turn
+        self.apply_spell_effects(self.foe)
+        self.foe.attacks(self, self.foe.damage)
         return self
 
-    def cast_spell(self, spell):
+    def cast_spell(self, spell, foe):
         # raise error if spell already in effect
         if spell in self.effects:
             raise ValueError(f"Spell {spell} should not have been an option.")
 
-        # add spell to effects
-        self.effects.append(spell)
+        # Instant Effect
+        if spell.duration == 0:
+            self.apply_spell_effect(spell, foe)
+
+        # Delayed Effect
+        else:
+            self.effects.append(spell)
+
         self.mana -= spell.cost
         self.mana_spent += spell.cost
-        #print(spell.cost, self.mana_spent)
+        return self
 
     def apply_spell_effects(self, foe):
-        # heal self
-        self.hp += sum([s.heal for s in self.effects])
-
-        # recharge self
-        self.mana += sum([s.recharge for s in self.effects])
-
-        # attack foe
-        self.attacks(foe)
-
-        # tick
-        for spell in self.effects:
-            spell.tick()
+        for effect in self.effects:
+            self.apply_spell_effect(effect, foe)
+            effect.tick()
 
         # clear expired spells
         self.effects = [e for e in self.effects if not e.expired]
+        return self
 
-    def attacks(self, foe):
-        damage = self.damage - foe.defense
+    def apply_spell_effect(self, spell, foe):
+        # heal self
+        self.hp += spell.heal
+
+        # recharge self
+        self.mana += spell.recharge
+
+        # attack foe
+        self.attacks(foe, spell.damage)
+
+        return self
+
+
+    def attacks(self, foe, damage):
+        if self.died:
+            return foe
+
         damage = max(damage, 1)
         foe.hp -= damage
+        #print(f"{self} attacks {foe} for {damage} damage")
         return foe
 
     def clone(self):
@@ -161,6 +177,9 @@ class Boss(Wizard):
     @property
     def defense(self):
         return 0
+
+    def __repr__(self):
+        return f"<Boss hp={self.hp} damage={self.damage}>"
 
 
 class Spell:
@@ -204,6 +223,9 @@ class Spell:
 
     def tick(self):
         self.ticks += 1
+
+    def __repr__(self):
+        return f"<Spell name={self.name} ticks={self.ticks} {self.effects}>"
 
 
 class SpellBook:
@@ -257,6 +279,7 @@ class AdventPuzzle:
 
         wizard = wizard.spend_least_mana_to_defeat(boss)
         assert wizard.mana_spent < 1212, f"{wizard.mana_spent} is too high"
+        assert wizard.mana_spent > 847, f"{wizard.mana_spent} is too low"
         return wizard.mana_spent
 
     @property
@@ -268,9 +291,41 @@ class AdventPuzzle:
     #
     @property
     def test1(self):
-        input = self.TEST_INPUT
-        print(input)
+        self.recreate_first_battle()
+        self.recreate_second_battle()
         return 'passed'
+
+    def recreate_first_battle(self):
+        wizard_hp = 10
+        wizard_mana = 250
+        wizard_book = SpellBook(SPELLS)
+        boss_hp = 13
+        boss_damage = 8
+
+        wizard = Wizard(wizard_hp, wizard_mana, wizard_book)
+        boss = Boss(boss_hp, boss_damage)
+
+        # Recreate battle 1
+        wizard.cast_spell(wizard_book.read('Poison'), boss)
+        wizard.apply_spell_effects(boss)
+        boss.attacks(wizard, boss.damage)
+        assert wizard.mana == 77, wizard
+        assert wizard.hp == 2, wizard
+        assert boss.hp == 10, boss
+        assert wizard.effects[0].ticks == 1
+
+        wizard.apply_spell_effects(boss)
+        wizard.cast_spell(wizard_book.read('Magic Missile'), boss)
+        wizard.apply_spell_effects(boss)
+        boss.attacks(wizard, boss.damage)
+        assert wizard.mana == 24, wizard
+        assert wizard.hp == 2, wizard
+        assert boss.hp == 0, boss
+        assert wizard.effects[0].ticks == 3
+        assert boss.died
+
+    def recreate_second_battle(self):
+        raise ValueError('TODO')
 
     @property
     def test2(self):
