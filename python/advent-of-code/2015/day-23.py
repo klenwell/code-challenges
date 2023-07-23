4b00bc5d1b9152e5f61dfeb3aa57f16e78ea8141
klenwell <klenwell@gmail.com>
2023-07-23 16:40:57 -0700

    Day 23 test 1 passes.

--- python/advent-of-code/2015/day-23.py
+++ python/advent-of-code/2015/day-23.py
@@ -7,11 +7,58 @@ from functools import cached_property
 from common import INPUT_DIR
 
 
+class Computer:
+    def __init__(self):
+        self.registers = {
+            'a': 0,
+            'b': 0
+        }
+        self.index = 0
+
+    def run(self, program):
+        instructions = program.split("\n")
+
+        while self.index < len(instructions):
+            method, args = self.parse(instructions[self.index])
+            method(*args)
+            print(self)
+
+        return self
+
+    def parse(self, instruction):
+        print(instruction)
+        method_attr, args = instruction.split(' ', 1)
+        method = getattr(self, method_attr)
+        return method, args.split(' ')
+
+    def inc(self, register):
+        self.registers[register] += 1
+        self.index += 1
+        return self
+
+    def jio(self, register, offset):
+        register = register[0]
+        is_odd = self.registers[register] % 2 == 1
+
+        if not is_odd:
+            self.index += 1
+        else:
+            self.index += int(offset)
+
+        return self
+
+    def __repr__(self):
+        return f"<Computer registers={self.registers} index={self.index}>"
+
+
 class AdventPuzzle:
     INPUT_FILE = path_join(INPUT_DIR, 'day-23.txt')
 
     TEST_INPUT = """\
-"""
+inc a
+jio a, +2
+tpl a
+inc a"""
 
     def solve(self):
         print(f"test 1 solution: {self.test1}")
@@ -25,7 +72,9 @@ class AdventPuzzle:
     @property
     def first(self):
         input = self.file_input
-        return input
+        pc = Computer()
+        pc.run(input)
+        return pc.registers['b']
 
     @property
     def second(self):
@@ -37,7 +86,9 @@ class AdventPuzzle:
     @property
     def test1(self):
         input = self.TEST_INPUT
-        print(input)
+        pc = Computer()
+        pc.run(input)
+        assert pc.registers['a'] == 2
         return 'passed'
 
     @property
