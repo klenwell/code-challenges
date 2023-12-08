657a6068c73ba75ed9540539abe1abef723be87a
klenwell <klenwell@gmail.com>
2023-12-07 21:34:42 -0800

    Solves Day 8 Part 1. Ran out of time for Part 2.

--- python/advent-of-code/2023/day-08.py
+++ python/advent-of-code/2023/day-08.py
@@ -7,11 +7,64 @@ from functools import cached_property
 from common import INPUT_DIR
 
 
+class DesertMap:
+    def __init__(self, input):
+        self.input = input.strip()
+
+    @cached_property
+    def instructions(self):
+        code, _ = self.input.split('\n\n')
+        return list(code.strip())
+
+    @cached_property
+    def nodes(self):
+        nodes = {}
+        _, block = self.input.split('\n\n')
+        lines = block.strip().split('\n')
+        for line in lines:
+            id, left, right = self.parse_node(line)
+            nodes[id] = (left, right)
+        return nodes
+
+    def walk_to_zzz(self):
+        steps = 0
+        node_id = 'AAA'
+
+        while node_id != 'ZZZ':
+            index = steps % len(self.instructions)
+            direction = self.instructions[index]
+            left, right = self.nodes[node_id]
+            node_id = left if direction == 'L' else right
+            steps += 1
+
+        return steps
+
+    def parse_node(self, line):
+        id, opts = line.split(' = ')
+        opts = opts.strip()[1:-1]
+        left, right = opts.split(', ')
+        return id.strip(), left.strip(), right.strip()
+
+
+class GhostMap(DesertMap):
+    @property
+    def nodes_ending_in_a(self):
+        node_ids = []
+        for node_id in self.nodes.keys():
+            if node_id[-1] == 'A':
+                node_ids.append(node_id)
+        return node_ids
+
+
 class AdventPuzzle:
     INPUT_FILE = path_join(INPUT_DIR, 'day-08.txt')
 
     TEST_INPUT = """\
-"""
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)"""
 
     def solve(self):
         print(f"test 1 solution: {self.test1}")
@@ -25,11 +78,15 @@ class AdventPuzzle:
     @property
     def first(self):
         input = self.file_input
-        return input
+        map = DesertMap(input)
+        steps = map.walk_to_zzz()
+        return steps
 
     @property
     def second(self):
-        pass
+        input = self.file_input
+        map = GhostMap(input)
+        print(map.nodes_ending_in_a)
 
     #
     # Tests
@@ -37,13 +94,29 @@ class AdventPuzzle:
     @property
     def test1(self):
         input = self.TEST_INPUT
-        print(input)
+        map = DesertMap(input)
+        print(map.nodes)
+        steps = map.walk_to_zzz()
+
+        assert steps == 6, steps
         return 'passed'
 
     @property
     def test2(self):
-        input = self.TEST_INPUT
-        print(input)
+        input = """\
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"""
+
+        map = GhostMap(input)
+        print(map.nodes_ending_in_a)
         return 'passed'
 
     #
