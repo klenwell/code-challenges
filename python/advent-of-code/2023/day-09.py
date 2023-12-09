fcd93d0cec75f0acf54aaa411cb71f50809c4c2f
klenwell <klenwell@gmail.com>
2023-12-08 21:38:08 -0800

    Solves Day 9 Part 1.

--- python/advent-of-code/2023/day-09.py
+++ python/advent-of-code/2023/day-09.py
@@ -7,11 +7,76 @@ from functools import cached_property
 from common import INPUT_DIR
 
 
+class Sensor:
+    def __init__(self, report):
+        self.report = report.strip()
+
+    @cached_property
+    def lines(self):
+        return self.report.split("\n")
+
+    @cached_property
+    def extrapolated_sum(self):
+        values = []
+        for line in self.lines:
+            extrapolator = Extrapolator(line)
+            print(line, extrapolator.next_value)
+            values.append(extrapolator.next_value)
+        return sum(values)
+
+    @cached_property
+    def backwards_sum(self):
+        values = []
+        for line in self.lines:
+            extrapolator = Extrapolator(line)
+            print(line, extrapolator.first_value)
+            values.append(extrapolator.first_value)
+        return sum(values)
+
+
+class Extrapolator:
+    def __init__(self, input):
+        self.input = input.strip()
+
+    @cached_property
+    def next_value(self):
+        next_value = 0
+        for sequence in self.sequences[1:]:
+            last_value = sequence[-1]
+            next_value += last_value
+            #print(sequence, last_value, next_value)
+        return next_value
+
+    @cached_property
+    def history(self):
+        return [int(n) for n in self.input.split()]
+
+    @cached_property
+    def sequences(self):
+        sequence = list(self.history)
+        sequences = [sequence]
+        while set(sequence) != set([0]):
+            sequence = self.derive_sequence(sequence)
+            sequences.append(sequence)
+        return list(reversed(sequences))
+
+    def derive_sequence(self, sequence):
+        derived = []
+        for n, value in enumerate(sequence):
+            if n == 0:
+                continue
+            diff = value - sequence[n-1]
+            derived.append(diff)
+        return derived
+
+
 class AdventPuzzle:
     INPUT_FILE = path_join(INPUT_DIR, 'day-09.txt')
 
     TEST_INPUT = """\
-"""
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
 
     def solve(self):
         print(f"test 1 solution: {self.test1}")
@@ -25,7 +90,8 @@ class AdventPuzzle:
     @property
     def first(self):
         input = self.file_input
-        return input
+        sensor = Sensor(input)
+        return sensor.extrapolated_sum
 
     @property
     def second(self):
@@ -37,13 +103,15 @@ class AdventPuzzle:
     @property
     def test1(self):
         input = self.TEST_INPUT
-        print(input)
+        sensor = Sensor(input)
+        assert sensor.extrapolated_sum == 114, sensor.extrapolated_sum
         return 'passed'
 
     @property
     def test2(self):
         input = self.TEST_INPUT
-        print(input)
+        sensor = Sensor(input)
+        assert sensor.backwards_sum == 2, sensor.backwards_sum
         return 'passed'
 
     #
