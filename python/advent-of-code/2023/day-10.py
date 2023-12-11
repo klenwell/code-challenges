97cc03fecf12e06602799728357f36b03d25659d
klenwell <klenwell@gmail.com>
2023-12-10 20:46:45 -0800

    Doing some preliminary analysis for Day 10 part 2. Not linking what I'm seeing so far.

--- python/advent-of-code/2023/day-10.py
+++ python/advent-of-code/2023/day-10.py
@@ -17,19 +17,27 @@ class PipeMaze(Grid):
 
     @cached_property
     def steps_to_farthest_point(self):
-        steps = []
+        return len(self.loop_pts) // 2
+
+    @cached_property
+    def loop_pts(self):
+        loop = []
         prev_pt = None
         pt = self.starting_pt
         back_at_start = False
 
         while not back_at_start:
             next_pt = self.move(pt, prev_pt)
-            steps.append(next_pt)
+            loop.append(next_pt)
             back_at_start = next_pt == self.starting_pt
             prev_pt = pt
             pt = next_pt
 
-        return len(steps) / 2
+        return loop
+
+    @cached_property
+    def junk_pts(self):
+        return set(self.pts) - set(self.loop_pts)
 
     @cached_property
     def starting_pt(self):
@@ -44,7 +52,7 @@ class PipeMaze(Grid):
 
         for npt in self.connected_pts(pt):
             if npt != prev_pt and npt in self.pts:
-                print((pt, self.grid[pt]), (npt, self.grid[npt]))
+                #print((pt, self.grid[pt]), (npt, self.grid[npt]))
                 return npt
 
         raise Exception(f"Unable to move from pt {pt}")
@@ -70,17 +78,6 @@ class PipeMaze(Grid):
 
         return pts
 
-    def pipes_connect(self, pipe1, pipe2):
-        disconnects = {
-            '|': '-',
-            '-': '|',
-            '7': '7',
-            'F': 'F',
-            'J': 'J',
-            'L': 'L',
-        }
-        return disconnects[pipe1] != pipe2
-
     def move_from_start_pt(self, pt):
         valid_moves = {
             'N': ('|', '7', 'F'),
@@ -95,13 +92,23 @@ class PipeMaze(Grid):
 
             dir = self.DIRS[(dx,dy)]
             val = self.grid[npt]
-
-            print(pt, npt, dir, val, val in valid_moves[dir])
+            #print(pt, npt, dir, val, val in valid_moves[dir])
 
             if val in valid_moves[dir]:
                 return npt
 
         raise Exception('First move failed!')
+
+    def xxx_pipes_connect(self, pipe1, pipe2):
+        disconnects = {
+            '|': '-',
+            '-': '|',
+            '7': '7',
+            'F': 'F',
+            'J': 'J',
+            'L': 'L',
+        }
+        return disconnects[pipe1] != pipe2
 
 
 class AdventPuzzle:
@@ -131,7 +138,16 @@ LJ..."""
 
     @property
     def second(self):
-        pass
+        input = self.file_input
+        maze = PipeMaze(input)
+
+        tile_count = len(maze.pts)
+        loop_count = len(maze.loop_pts)
+        junk_count = len(maze.junk_pts)
+        print(tile_count, loop_count, junk_count)
+        assert tile_count == loop_count + junk_count, loop_count + junk_count
+
+        return len(maze.enclosed_tiles)
 
     #
     # Tests
@@ -148,8 +164,28 @@ LJ..."""
 
     @property
     def test2(self):
+        test_input = """\
+OF----7F7F7F7F-7OOOO
+O|F--7||||||||FJOOOO
+O||OFJ||||||||L7OOOO
+FJL7L7LJLJ||LJIL-7OO
+L--JOL7IIILJS7F-7L7O
+OOOOF-JIIF7FJ|L7L7L7
+OOOOL7IF7||L7|IL7L7|
+OOOOO|FJLJ|FJ|F7|OLJ
+OOOOFJL-7O||O||||OOO
+OOOOL---JOLJOLJLJOOO"""
         input = self.TEST_INPUT
-        print(input)
+        maze = PipeMaze(input)
+
+        tile_count = len(maze.pts)
+        loop_count = len(maze.loop_pts)
+        junk_count = len(maze.junk_pts)
+        print(tile_count, loop_count, junk_count)
+        assert tile_count == loop_count + junk_count, loop_count + junk_count
+
+        assert len(maze.enclosed_tiles) == 8, maze.enclosed_tiles
+
         return 'passed'
 
     #
