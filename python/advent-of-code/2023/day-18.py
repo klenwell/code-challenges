5c92b63ac410a3b8614d39de57c568af120bc537
klenwell <klenwell@gmail.com>
2024-01-12 22:28:13 -0800

    Solved Day 18 test 2 but still fails part 2.

--- python/advent-of-code/2023/day-18.py
+++ python/advent-of-code/2023/day-18.py
@@ -6,6 +6,9 @@ from os.path import join as path_join
 from functools import cached_property
 from common import INPUT_DIR, info
 
+#from models.day_18.lava_pit import SmallLavaPit, BigLavaPit
+from models.day_18 import polygon as poly
+
 
 DIRS = {
     'U': (0, -1),
@@ -15,16 +18,71 @@ DIRS = {
 }
 
 
-class LavaPit:
+
+class ParceledLavaPit:
     def __init__(self, input):
         self.input = input.strip()
-        self.holes = {}
+
+    @cached_property
+    def xs(self):
+        return sorted(list(set([x for x,_ in self.pts])))
+
+    @cached_property
+    def ys(self):
+        return sorted(list(set([y for _,y in self.pts])))
 
     @cached_property
     def cubic_meters(self):
-        self.excavate()
-        self.fill()
-        return len(self.holes)
+        area = 0
+        for key, parcel in self.parcel_grid.items():
+            row_num, col_num = key
+
+            if not self.is_interior_parcel(parcel):
+                continue
+
+            left_neighbor = self.parcel_grid.get((row_num, col_num-1))
+            upper_neighbor = self.parcel_grid.get((row_num-1, col_num))
+            upper_left_neighbor = self.parcel_grid.get((row_num-1, col_num-1))
+
+            area += parcel.area
+
+            subtract_left_edge = left_neighbor and self.is_interior_parcel(left_neighbor)
+            subtract_upper_edge = upper_neighbor and self.is_interior_parcel(upper_neighbor)
+            add_back_corner = subtract_left_edge and subtract_upper_edge
+
+            shifted = False
+
+            if subtract_left_edge:
+                area -= parcel.height
+                shifted = True
+            if subtract_upper_edge:
+                area -= parcel.width
+                shifted = True
+
+            if add_back_corner:
+                area += 1
+
+            if not shifted:
+                if upper_left_neighbor and self.is_interior_parcel(upper_left_neighbor):
+                    print(parcel, upper_left_neighbor)
+                    area -= 1
+                    breakpoint()
+        return area
+
+    def is_interior_parcel(self, parcel):
+        info(f"is_interior_parcel {parcel}", 10000)
+        def is_odd(num):
+            return num % 2 == 1
+        edges_above = poly.count_edges_above_pt(self.horizontal_edges, parcel.mid_pt)
+        edges_below = poly.count_edges_below_pt(self.horizontal_edges, parcel.mid_pt)
+        edges_to_left = poly.count_edges_left_of_pt(self.vertical_edges, parcel.mid_pt)
+        edges_to_right = poly.count_edges_right_of_pt(self.vertical_edges, parcel.mid_pt)
+        return all([
+            is_odd(edges_above),
+            is_odd(edges_below),
+            is_odd(edges_to_left),
+            is_odd(edges_to_right)
+        ])
 
     @cached_property
     def instructions(self):
@@ -37,187 +95,32 @@ class LavaPit:
             instructions.append(instruction)
         return instructions
 
-    def excavate(self):
-        hole = Hole(0, 0, None)
-        for dir, size, rgb in self.instructions:
-            dx, dy = DIRS[dir]
-            for n in range(size):
-                hole.x += dx
-                hole.y += dy
-                hole.rgb = rgb
-                self.dig_hole(hole)
-                info(f"excavating {hole}", 1000)
-
-    def fill(self):
-        start_pt = self.find_inner_pt()
-        queue = [start_pt]
-
-        while len(queue) > 0:
-            x, y = queue.pop()
-            hole = Hole(x, y, None)
-            self.dig_hole(hole)
-            info(f"filling {hole}", 10000)
-
-
-            for npt in self.neighbors(hole.pt):
-                if npt not in self.holes:
-                    queue.append(npt)
-
-    def find_inner_pt(self):
-        iy = min([y for _,y in self.holes.keys()])
-        ix = min([x for x,y in self.holes.keys() if y == iy])
-
-
-        hole = self.holes.get((ix, iy), False)
-
-        while hole:
-            print(hole)
-            ix = hole.x + 1
-            iy = hole.y + 1
-            hole = self.holes.get((ix, iy), False)
-
-        print('start', ix, iy)
-        #breakpoint()
-        return (ix, iy)
-
-    def neighbors(self, pt):
-        x, y = pt
-        for dx, dy in DIRS.values():
-            yield (x+dx, y+dy)
-
-    def dig_hole(self, hole):
-        new_hole = Hole(hole.x, hole.y, hole.rgb)
-        self.holes[new_hole.pt] = new_hole
-
-
-class Hole:
-    def __init__(self, x, y, rgb):
-        self.x = x
-        self.y = y
-        self.rgb = rgb
-
-    @property
-    def pt(self):
-        return (self.x, self.y)
-
-    def __repr__(self):
-        return f"<Hole {self.pt} rgb={self.rgb}>"
-
-
-class BigLavaPit(LavaPit):
-    @cached_property
-    def cubic_meters(self):
-        area = 0
-        for parcel in self.interior_parcels:
-            area += parcel.area
-        return area - self.overlapping_edges_area - self.overlapping_panel_corners - 2
-
-    @cached_property
-    def interior_parcels(self):
-        return [parcel for parcel in self.parcels if self.contains_parcel(parcel)]
-
-    @cached_property
-    def edge_counts(self):
-        edge_counts = {}
-        pt_counts = {}
-        for parcel in self.interior_parcels:
-            for edge in parcel.edges:
-                if edge in edge_counts:
-                    edge_counts[edge] += 1
-                else:
-                    edge_counts[edge] = 1
-        return edge_counts
-
-    @cached_property
-    def overlapping_edges(self):
-        edges = []
-        for edge, count in self.edge_counts.items():
-            if count > 1:
-                edges.append(edge)
-                assert count == 2, f"count for edge > 1 ({count})"
-        return edges
-
-    @cached_property
-    def overlapping_corners(self):
-        pt_counts = {}
-        for edge in self.edge_counts.keys():
-            pt1, pt2 = edge
-            pt1_count = pt_counts.get(pt1, 0)
-            pt2_count = pt_counts.get(pt2, 0)
-            pt_counts[pt1] = pt1_count + 1
-            pt_counts[pt2] = pt2_count + 1
-        return pt_counts
-
-    @cached_property
-    def overlapping_panel_corners(self):
-        # These are kitty-cornered panels with a single over-lapping corner hole that will
-        # be overcounted. Or panels that share a corner without sharing an edge.
-        count = 0
-        pt_parcels = {}
-        for parcel in self.interior_parcels:
-            for pt in parcel.pts:
-                if pt in pt_parcels:
-                    pt_parcels[pt].append(parcel)
-                else:
-                    pt_parcels[pt] = [parcel]
-
-        import itertools as it
-        for pt, parcels in pt_parcels.items():
-            if len(parcels) < 2:
-                continue
-
-            compared = []
-            print('ids', [id(p) for p in parcels])
-
-            for p1, p2 in it.permutations(parcels, 2):
-                pair_key = sorted([id(p1), id(p2)])
-                if pair_key in compared:
-                    continue
-
-                compared.append(pair_key)
-                #print(pair_key)
-                if not p1.shares_edge_with(p2):
-                    print('hit', (p1, p2))
-                    count += 1
-        #breakpoint()
-        return count
-
-    @cached_property
-    def overlapping_edges_area(self):
-        area = 0
-        for edge in self.overlapping_edges:
-            (x1, y1), (x2, y2) = edge
-            dx = abs(x2 - x1)
-            dy = abs(y2 - y1)
-            assert 0 in [dx, dy], (dx, dy)
-            area += dx + dy
-        return area
-
-    @cached_property
-    def parcels(self):
-        parcels = []
-        for i, x in enumerate(self.xs[:-1]):
-            for j, y in enumerate(self.ys[:-1]):
-                next_x = self.xs[i+1]
-                next_y = self.ys[j+1]
-                pts = [
-                    (x, y),
-                    (next_x, y),
-                    (next_x, next_y),
-                    (x, next_y)
-                ]
-                parcel = Parcel(pts)
-                parcels.append(parcel)
-                info(parcel, 1000)
-        return parcels
-
     @cached_property
     def pts(self):
         pts = [(0, 0)]
         for _, _, rgb in self.instructions:
             pt = self.rgb_to_next_pt(pts[-1], rgb)
             pts.append(pt)
+        breakpoint()
         return pts
+
+    @cached_property
+    def parcel_grid(self):
+        parcels = {}
+        for row_num, y in enumerate(self.ys[:-1]):
+            for col_num, x in enumerate(self.xs[:-1]):
+                next_x = self.xs[col_num+1]
+                next_y = self.ys[row_num+1]
+                pts = [
+                    (x, y),
+                    (next_x, y),
+                    (next_x, next_y),
+                    (x, next_y)
+                ]
+                parcel = Parcel(pts, self)
+                parcels[(row_num, col_num)] = parcel
+                info(parcel, 1000)
+        return parcels
 
     @cached_property
     def horizontal_edges(self):
@@ -254,92 +157,71 @@ class BigLavaPit(LavaPit):
 
         # https://stackoverflow.com/a/209550/1093087
         distance = int(hex, 16)
-        #print(rgb, dir, distance)
 
         px, py = previous_pt
         x = px + (dx * distance)
         y = py + (dy * distance)
         pt = (x, y)
+        print(rgb, dir, distance, pt)
         return pt
-
-    @cached_property
-    def xs(self):
-        return sorted(list(set([x for x,_ in self.pts])))
-
-    @cached_property
-    def ys(self):
-        return sorted(list(set([y for _,y in self.pts])))
-
-    def contains_parcel(self, parcel):
-        info(f"contains {parcel}", 10000)
-        under_top_edge = self.pt_under_top_edge(parcel.mid_pt)
-        above_bottom_edge = self.pt_above_bottom_edge(parcel.mid_pt)
-        right_of_left_edge = self.pt_right_of_left_edge(parcel.mid_pt)
-        left_of_right_edge = self.pt_left_of_right_edge(parcel.mid_pt)
-        return all([under_top_edge, above_bottom_edge, right_of_left_edge, left_of_right_edge])
-
-    def pt_under_top_edge(self, pt):
-        x, y = pt
-        for edge in self.horizontal_edges:
-            pt1, pt2 = edge
-            e1x, e1y = pt1
-            e2x, e2y = pt2
-            assert e1y == e2y, edge
-            min_x, max_x = sorted([e1x, e2x])
-            between_xs = min_x <= x <= max_x
-            under_edge = y > e1y
-            if between_xs and under_edge:
-                return True
-        return False
-
-    def pt_above_bottom_edge(self, pt):
-        x, y = pt
-        for edge in self.horizontal_edges:
-            pt1, pt2 = edge
-            e1x, e1y = pt1
-            e2x, e2y = pt2
-            assert e1y == e2y, edge
-            min_x, max_x = sorted([e1x, e2x])
-            between_xs = min_x <= x <= max_x
-            above_edge = y < e1y
-            if between_xs and above_edge:
-                return True
-        return False
-
-    def pt_right_of_left_edge(self, pt):
-        x, y = pt
-        for edge in self.vertical_edges:
-            pt1, pt2 = edge
-            e1x, e1y = pt1
-            e2x, e2y = pt2
-            assert e1x == e2x, edge
-            min_y, max_y = sorted([e1y, e2y])
-            between_ys = min_y <= y <= max_y
-            right_of_edge = x > e1x
-            if between_ys and right_of_edge:
-                return True
-        return False
-
-    def pt_left_of_right_edge(self, pt):
-        x, y = pt
-        for edge in self.vertical_edges:
-            pt1, pt2 = edge
-            e1x, e1y = pt1
-            e2x, e2y = pt2
-            assert e1x == e2x, edge
-            min_y, max_y = sorted([e1y, e2y])
-            between_ys = min_y <= y <= max_y
-            left_of_edge = x < e1x
-            if between_ys and left_of_edge:
-                return True
-        return False
 
 
 class Parcel:
-    def __init__(self, pts):
+    def __init__(self, pts, pit):
         self.pts = pts
-        self.xs = [x for x,_ in pts]
-        self.ys = [y for _,y in pts]
+        self.pit = pit
+
+    @property
+    def area(self):
+        return self.width * self.height
+
+    @property
+    def width(self):
+        # Edge case: consider box with pts (0,0) -> (4,0) -> (4,4) -> (0,0). Subtracting
+        # coordinates gives you 4x4 when it should be 5x5.
+        return max(self.xs) - self.min_x + 1
+
+    @property
+    def height(self):
+        return max(self.ys) - self.min_y + 1
+
+    @property
+    def xs(self):
+        return [x for x,_ in self.pts]
+
+    @property
+    def ys(self):
+        return [y for _,y in self.pts]
+
+    @property
+    def min_x(self):
+        return min(self.xs)
+
+    @property
+    def min_y(self):
+        return min(self.ys)
+
+    def shift_left(self):
+        pts = []
+        for x, y in self.pts:
+            if x == self.min_x:
+                new_pt = (x+1, y)
+            else:
+                new_pt = (x, y)
+            pts.append(new_pt)
+        self.pts = pts
+        return self
+
+    def shift_down(self):
+        pts = []
+        for x, y in self.pts:
+            if y == self.min_y:
+                new_pt = (x, y+1)
+            else:
+                new_pt = (x, y)
+            pts.append(new_pt)
+        self.pts = pts
+        return self
 
     @cached_property
     def edges(self):
@@ -373,6 +255,20 @@ class Parcel:
         return f"<Parcel {self.pts} mid_pt={self.mid_pt} area={self.area}>"
 
 
+class Hole:
+    def __init__(self, x, y, rgb):
+        self.x = x
+        self.y = y
+        self.rgb = rgb
+
+    @property
+    def pt(self):
+        return (self.x, self.y)
+
+    def __repr__(self):
+        return f"<Hole {self.pt} rgb={self.rgb}>"
+
+
 class AdventPuzzle:
     INPUT_FILE = path_join(INPUT_DIR, 'day-18.txt')
 
@@ -404,10 +300,11 @@ U 2 (#7a21e3)"""
     @property
     def second(self):
         input = self.file_input
-        pit = BigLavaPit(input)
-        print(len(pit.parcels))
+        pit = ParceledLavaPit(input)
+        #print(len(pit.parcels))
         assert pit.cubic_meters != 98956108013068, pit.cubic_meters
         assert pit.cubic_meters != 98956107977040, pit.cubic_meters
+        assert pit.cubic_meters != 87716969654495, pit.cubic_meters
         return pit.cubic_meters
 
     #
@@ -423,7 +320,7 @@ U 2 (#7a21e3)"""
     @property
     def test2(self):
         input = self.TEST_INPUT
-        pit = BigLavaPit(input)
+        pit = ParceledLavaPit(input)
 
         def errs(val, expected):
             diff = expected - val
@@ -443,8 +340,8 @@ U 2 (#7a21e3)"""
             return file.read().strip()
 
     def solve(self):
-        print(f"test 1 solution: {self.test1}")
-        print(f"Part 1 Solution: {self.first}")
+        #print(f"test 1 solution: {self.test1}")
+        #print(f"Part 1 Solution: {self.first}")
         print(f"test 2 solution: {self.test2}")
         print(f"Part 2 Solution: {self.second}")
 
---
+++ python/advent-of-code/2023/models/day_18/polygon.py
@@ -0,0 +1,79 @@
+
+def count_edges_above_pt(edges, pt):
+    edge_count = 0
+    for edge in edges:
+        if pt_is_under_edge(pt, edge):
+            edge_count += 1
+    return edge_count
+
+
+def count_edges_below_pt(edges, pt):
+    edge_count = 0
+    for edge in edges:
+        if pt_is_above_edge(pt, edge):
+            edge_count += 1
+    return edge_count
+
+
+def count_edges_left_of_pt(edges, pt):
+    edge_count = 0
+    for edge in edges:
+        if pt_is_right_of_edge(pt, edge):
+            edge_count += 1
+    return edge_count
+
+
+def count_edges_right_of_pt(edges, pt):
+    edge_count = 0
+    for edge in edges:
+        if pt_is_left_of_edge(pt, edge):
+            edge_count += 1
+    return edge_count
+
+
+def pt_is_above_edge(pt, edge):
+    x, y = pt
+    pt1, pt2 = edge
+    e1x, e1y = pt1
+    e2x, _ = pt2
+    min_x, max_x = sorted([e1x, e2x])
+
+    between_xs = min_x <= x <= max_x
+    above_edge = y < e1y
+    return between_xs and above_edge
+
+
+def pt_is_under_edge(pt, edge):
+    x, y = pt
+    pt1, pt2 = edge
+    e1x, e1y = pt1
+    e2x, _ = pt2
+    min_x, max_x = sorted([e1x, e2x])
+
+    between_xs = min_x <= x <= max_x
+    under_edge = y > e1y
+    return between_xs and under_edge
+
+
+def pt_is_right_of_edge(pt, edge):
+    x, y = pt
+    pt1, pt2 = edge
+    e1x, e1y = pt1
+    _, e2y = pt2
+    min_y, max_y = sorted([e1y, e2y])
+
+    between_ys = min_y <= y <= max_y
+    right_of_edge = x > e1x
+    return between_ys and right_of_edge
+
+
+def pt_is_left_of_edge(pt, edge):
+    x, y = pt
+    pt1, pt2 = edge
+    e1x, e1y = pt1
+    _, e2y = pt2
+    min_y, max_y = sorted([e1y, e2y])
+
+    between_ys = min_y <= y <= max_y
+    left_of_edge = x < e1x
+    return between_ys and left_of_edge
