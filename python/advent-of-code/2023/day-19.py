7bc9db4322af8822daf8a86aa589fb600e95b3c5
klenwell <klenwell@gmail.com>
2024-01-16 07:57:43 -0800

    Fails test 2. Answer much too high.

--- python/advent-of-code/2023/day-19.py
+++ python/advent-of-code/2023/day-19.py
@@ -4,8 +4,11 @@ https://adventofcode.com/2023/day/19
 """
 from os.path import join as path_join
 from functools import cached_property
-from common import INPUT_DIR
-import json
+from common import INPUT_DIR, info
+import math
+
+
+MAX_VALUE = 4000
 
 
 class Extract:
@@ -17,7 +20,7 @@ class Extract:
         workflows = {}
         input, _ = self.input.split('\n\n')
         for line in input.split('\n'):
-            workflow = Workflow(line)
+            workflow = ExtractWorkflow(self, line)
             workflows[workflow.name] = workflow
         return workflows
 
@@ -34,11 +37,36 @@ class Extract:
     def sum(self):
         sum = 0
         for rating in self.ratings:
-            if self.is_accepted(rating):
+            if self.rating_is_accepted(rating):
                 sum += rating.sum
         return sum
 
-    def is_accepted(self, rating):
+    @cached_property
+    def distinct_combinations(self):
+        return sum([path.combos for path in self.accepted_paths])
+
+    @cached_property
+    def accepted_paths(self):
+        # A WorkflowPath is made up of RuleSteps
+        # To branch, take last step and find next workflow
+        accepted_paths = []
+        first_workflow = self.workflows['in']
+        path_heap = [Path(first_workflow)]
+
+        while path_heap:
+            info(len(path_heap), 100)
+            path = path_heap.pop()
+            for new_path in path.branches:
+                if new_path.accepted:
+                    print('ACCEPT', new_path)
+                    accepted_paths.append(new_path)
+                elif path.in_progress:
+                    path_heap.append(new_path)
+
+        breakpoint()
+        return accepted_paths
+
+    def rating_is_accepted(self, rating):
         stops = list('AR')
         result = 'in'
 
@@ -49,8 +77,69 @@ class Extract:
         return result == 'A'
 
 
-class Workflow:
-    def __init__(self, input):
+class Path:
+    def __init__(self, workflow, steps=None):
+        self.workflow = workflow
+        if not steps:
+            self.steps = []
+        else:
+            self.steps = list(steps)
+
+    @cached_property
+    def combos(self):
+        return math.prod([step.combos for step in self.steps])
+
+    @cached_property
+    def workflows(self):
+        return self.workflow.extract.workflows
+
+    @cached_property
+    def branches(self):
+        new_paths = []
+        continuing_step = None
+
+        for rule in self.workflow.rules:
+            true_step, false_step = rule.to_steps(continuing_step)
+            continuing_step = false_step
+            new_path = self.add_step(true_step)
+            new_paths.append(new_path)
+
+        return new_paths
+
+    @property
+    def last_step(self):
+        if not self.steps:
+            return None
+        return self.steps[-1]
+
+    @property
+    def result(self):
+        if not self.last_step:
+            return None
+        return self.last_step.result
+
+    @property
+    def accepted(self):
+        if not self.last_step:
+            return False
+        return self.result == 'A'
+
+    @property
+    def in_progress(self):
+        return self.result not in ('A', 'R')
+
+    def add_step(self, step):
+        self.steps.append(step)
+        next_workflow = self.workflows.get(step.destination)
+        return Path(next_workflow, self.steps)
+
+    def __repr__(self):
+        return f"<Path result={self.result} steps={self.steps}>"
+
+
+class ExtractWorkflow:
+    def __init__(self, extract, input):
+        self.extract = extract
         self.input = input.strip()
 
     @cached_property
@@ -64,7 +153,7 @@ class Workflow:
         _, raw = self.input.split('{')
         rules_input = raw[:-1]
         for rule_input in rules_input.split(','):
-            rule = WorkflowRule(rule_input)
+            rule = WorkflowRule(self, rule_input)
             rules.append(rule)
         return rules
 
@@ -76,7 +165,8 @@ class Workflow:
 
 
 class WorkflowRule:
-    def __init__(self, input):
+    def __init__(self, workflow, input):
+        self.workflow = workflow
         self.input = input.strip()
 
     @cached_property
@@ -109,6 +199,17 @@ class WorkflowRule:
             return None
         return int(self.condition[2:])
 
+    @cached_property
+    def true_cases(self):
+        if self.operator == '<':
+            return self.value - 1
+        else:
+            return MAX_VALUE - self.value
+
+    @cached_property
+    def false_cases(self):
+        return MAX_VALUE - self.true_cases
+
     def applies_to_rating(self, rating):
         if not self.condition:
             return True
@@ -120,6 +221,34 @@ class WorkflowRule:
             return rating_value < self.value
         else:
             return Exception(f"Unexpected rule comparator: {self.operator}")
+
+    def to_steps(self, incomplete_step):
+        combos = 1
+        if incomplete_step:
+            combos = incomplete_step.combos
+        if not self.condition:
+            true_step = RuleStep(self, self.result, combos)
+            false_step = None
+        else:
+            true_step = RuleStep(self, self.result, combos * self.true_cases)
+            false_step = RuleStep(self, None, combos * self.false_cases)
+        return true_step, false_step
+
+
+class RuleStep:
+    def __init__(self, rule, destination, combos):
+        self.rule = rule
+        self.origin = self.rule.workflow.name
+        self.destination = destination
+        self.combos = combos
+
+    @cached_property
+    def result(self):
+        return self.destination
+
+    def __repr__(self):
+        route = f"{self.origin}->{self.destination}"
+        return f"<RuleStep {route} combos={self.combos}>"
 
 
 class PartRating:
@@ -185,14 +314,14 @@ hdj{m>838:A,pv}
     def test1(self):
         input = self.TEST_INPUT
         extract = Extract(input)
-
         assert extract.sum == 19114
         return 'passed'
 
     @property
     def test2(self):
         input = self.TEST_INPUT
-        print(input)
+        extract = Extract(input)
+        assert extract.distinct_combinations == 167409079868000, extract.distinct_combinations
         return 'passed'
 
     #
